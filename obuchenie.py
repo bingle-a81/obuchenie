7aad638353d49f973932e9ac494ce129ab6650b2
Al <breath44@yandex.ru>
2023-03-16 06:47:25 +0300

    2

--- obuchenie.py
+++ obuchenie.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import sec1
 import fr
 
@@ -6,3 +7,14 @@ import fr
 if __name__ == '__main__':
     sec1.d()
 
+=======
+a =int(input())
+b =int(input())
+n =int(input())
+
+
+
+s=a*n*60+b*n
+
+print(s//3600,s%3600//60,s%60)
+>>>>>>> 3c6fd4b (2)
