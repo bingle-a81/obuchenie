243e4b7eaee30c8e7fa0f0dd3f0decc755536d36
STUD <breath44@yande.ru>
2023-11-14 15:22:44 +0300

    rrr

--- obuchenie.py
+++ obuchenie.py
@@ -1,47 +1,23 @@
-from string import ascii_lowercase, digits
+class ListObject:
+    def __init__(self,next_obj,data):
+        self.next_obj = next_obj
+        self.data = data
 
-CHARS = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя " + ascii_lowercase
-CHARS_CORRECT = CHARS + CHARS.upper() + digits
-
-class TextInput:
-    
-    def __init__(self,name,size=10,):
-        self.name = name
-        self.size = size
-        if not (self.check_name(self.name) and 3<self.size<50):
-            raise ValueError("некорректное поле name")
-
-    def get_html(self):
-        return f"<p class='login'>{self.name}: <input type='text' size={self.size} />"
-    
-    @classmethod
-    def check_name(cls, name):
-        if not all(x for x in name if x in CHARS_CORRECT):
-            return
-
-    
-
-class PasswordInput:
-
-    def __init__(self,name,size=10,):
-        self.name = name
-        self.size = size
-
-    def get_html(self):
-        return f"<p class='password'>{self.name}: <input type='text' size={self.size} />"
-
-# здесь объявляйте классы TextInput и PasswordInput
+    def link(self,obj):
+        self.next_obj = obj
 
 
-class FormLogin:
-    def __init__(self, lgn, psw):
-        self.login = lgn
-        self.password = psw
+lst_in = ['1. Первые шаги в ООП',
+          '1.1 Как правильно проходить этот курс',
+          '1.2 Концепция ООП простыми словами',
+          '1.3 Классы и объекты. Атрибуты классов и объектов',
+          '1.4 Методы классов. Параметр self',
+          '1.5 Инициализатор init и финализатор del',
+          '1.6 Магический метод new. Пример паттерна Singleton',
+          '1.7 Методы класса (classmethod) и статические методы (staticmethod)']
 
-    def render_template(self):
-        return "\n".join(['<form action="#">', self.login.get_html(), self.password.get_html(), '</form>'])
+head_obj=ListObject(None,lst_in[0])
 
+for i in range(1,len(lst_in)):
+    ListObject(head_obj,lst_in[i])
 
-# эти строчки не менять
-login = FormLogin(TextInput("Логин"), PasswordInput("Пароль"))
-html = login.render_template()
