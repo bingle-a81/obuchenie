b06925f8b3b0cd39edf74d65901f04256b800d79
Al <breath44@yandex.ru>
2023-11-14 22:14:24 +0300

    55

--- obuchenie.py
+++ obuchenie.py
@@ -1,23 +1,30 @@
-class ListObject:
-    def __init__(self,next_obj,data):
-        self.next_obj = next_obj
-        self.data = data
+import random
 
-    def link(self,obj):
-        self.next_obj = obj
+class Cell:
+    def __init__(self,around_mines:int, mine:int):
+        self.around_mines = around_mines
+        self.mine = mine
+        self.fl_open=True
 
 
-lst_in = ['1. Первые шаги в ООП',
-          '1.1 Как правильно проходить этот курс',
-          '1.2 Концепция ООП простыми словами',
-          '1.3 Классы и объекты. Атрибуты классов и объектов',
-          '1.4 Методы классов. Параметр self',
-          '1.5 Инициализатор init и финализатор del',
-          '1.6 Магический метод new. Пример паттерна Singleton',
-          '1.7 Методы класса (classmethod) и статические методы (staticmethod)']
+class GamePole:
+    def __init__(self,N:int,M:int):
+        self.N=N
+        self.M=M
+        self.pole=[['#' for i in range(self.N)] for j in range(self.N)]
+        for i in range(self.M):
+            self.pole[random.randint(0,self.N-1)][random.randint(0,self.N-1)]='*'
+    
+       
+    
+    def show(self):
+        for i in range(self.N):
+            for j in range(self.N):
+                print(self.pole[i][j],end=' ')
+            print('')
 
-head_obj=ListObject(None,lst_in[0])
 
-for i in range(1,len(lst_in)):
-    ListObject(head_obj,lst_in[i])
-
+N=10
+M=12
+pole_game=GamePole(N,M)
+pole_game.show()
