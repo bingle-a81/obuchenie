8d85742bab4faf598052ebd625d48aa82acff2b6
Al <breath44@yandex.ru>
2023-11-15 07:16:30 +0300

    rt

--- obuchenie.py
+++ obuchenie.py
@@ -1,30 +1,3 @@
-import random
-
-class Cell:
-    def __init__(self,around_mines:int, mine:int):
-        self.around_mines = around_mines
-        self.mine = mine
-        self.fl_open=True
-
-
-class GamePole:
-    def __init__(self,N:int,M:int):
-        self.N=N
-        self.M=M
-        self.pole=[['#' for i in range(self.N)] for j in range(self.N)]
-        for i in range(self.M):
-            self.pole[random.randint(0,self.N-1)][random.randint(0,self.N-1)]='*'
-    
-       
-    
-    def show(self):
-        for i in range(self.N):
-            for j in range(self.N):
-                print(self.pole[i][j],end=' ')
-            print('')
-
-
-N=10
-M=12
-pole_game=GamePole(N,M)
-pole_game.show()
+list1 = [[1, 8, 7, 4], [1, 3, 4, 5], [2, 7, 2], [2, 6, 7, 8]]
+list1.reverse()
+print(list1)
