1975dae43eba6a4eb294449cf660f1022cc17b6d
Al <breath44@yandex.ru>
2023-11-15 22:49:39 +0300

    er

--- obuchenie.py
+++ obuchenie.py
@@ -1,45 +1,21 @@
-TYPE_OS = 1 
-# 1 - Windows; 2 - Linux
+from string import ascii_lowercase, digits
+import  re
 
-class DialogWindows:
-    name_class = "DialogWindows"
+class CardCheck:
+    @staticmethod
+    def check_card_number(number):
+        if not re.fullmatch(r'^[0-9]{4}\-[0-9]{4}\-[0-9]{4}\-[0-9]{4}', number):
+            return False
+        return True
+    
+    @staticmethod
+    def check_name(name):
+        if not re.fullmatch(r'[A-Z]+\s+[A-Z]', name):
+            return False
+        return True
+        
+print(re.fullmatch(r'[0-9]{2}', "12"))
+# is_number = CardCheck.check_card_number("1234-5678-9012-0000")
+# is_name = CardCheck.check_name("SERGEI BALAKIREV")
+# print(is_number, is_name)
 
-
-class DialogLinux:
-    name_class = "DialogLinux"
-
-class Dialog:
-    def __new__(cls,*args):
-        if TYPE_OS==1:
-            d=DialogWindows()
-            setattr(d,'name',args)
-        elif TYPE_OS==2:
-            d=DialogLinux()
-            setattr(d,'name',args)
-        return d
-
-
-print(DialogWindows.name_class)
-
-
-
-
-
-dlg = Dialog('название')
-print(dlg.name)
-
-TYPE_OS = 1
-dlg_1 = Dialog("123")
-TYPE_OS = 2
-dlg_2 = Dialog("1234")
-
-assert isinstance(dlg_1, DialogWindows) and isinstance(dlg_2, DialogLinux), "создаваемые объекты не соответствуют нужным классам DialogWindows или DialogLinux"
-
-assert dlg_1.name == "123", "неверное значение локального атрибута name класса DialogWindows"
-assert dlg_2.name == "1234", "неверное значение локального атрибута name класса DialogLinux"
-
-d1 = Dialog("12")
-d2 = Dialog("123")
-
-assert d1.name == "12" and d2.name == "123", "неверные значения в локальных атрибутах name разных объектов класса DialogLinux"
-            
