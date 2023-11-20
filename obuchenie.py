2abceb20c01e0a7de0e0ff41670de3a85d789b81
Al <breath44@yandex.ru>
2023-11-20 21:22:00 +0300

    58

--- obuchenie.py
+++ obuchenie.py
@@ -1,134 +1,17 @@
-class ObjList:
-    def __init__(self,data):
-        self.__next=None
-        self.__prev=None
-        self.__data=data
+class Car:
 
-CHARS=ascii_letters+ascii_letters.upper()+digits+'_.'
+    @property
+    def model(self):
+        return  self.__model
+    
+    @model.setter
+    def model(self,model):
+        if all([type(model)==str,1<len(model)<101,]):
+            self.__model=model
 
-class EmailValidator():
-    def __new__(cls,*args,**kwargs) :
-        return None
+car = Car()
+car.model = "Toyota"
+print(car.__dict__)
+
+
         
-    @classmethod
-    def get_random_email(cls):
-        return ''.join(CHARS[random.randint(0,len(CHARS)-1)] for x in range(random.randint(1,100)))+'@gmail.com'
-
-    def get_next(self):
-        return self.__next
-    
-    def get_prev(self):
-        return self.__prev
-    
-    def get_data(self):
-        return self.__data
-
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-        
-    def add_obj(self, obj:ObjList):
-        if self.head is None:
-            self.head=obj
-            self.tail=obj
-            return
-        n=self.head
-        while n.get_next() :
-            n=n.get_next()
-        new_obj=obj
-        n.set_next(new_obj)
-        self.tail=new_obj
-        new_obj.set_prev(n)
-        return
-
-    def remove_obj(self):
-        if self.head is None:
-            print("List has no element")
-            return
-        if self.head.get_next() is None:
-            self.head=None
-            return
-        n=self.head
-        while n.get_next():
-            n=n.get_next()
-        n.get_prev().set_next(None)
-
-
-
-    def get_data(self):
-        d=[]
-        if self.head is None:
-            return []
-        else:
-            n=self.head
-            d.append(n.get_data())
-            while n.get_next():                    
-                n=n.get_next()
-                d.append(n.get_data())
-        return d
-
-
-
-
-ls = LinkedList()
-
-ls.add_obj(ObjList("данные 1"))
-ls.add_obj(ObjList("данные 2"))
-ls.add_obj(ObjList("данные 3"))
-ls.add_obj(ObjList("данные 34"))
-
-assert ls.get_data() == ['данные 1', 'данные 2', 'данные 3', 'данные 34'], "метод get_data вернул неверные данные"
-
-ls_one = LinkedList()
-ls_one.add_obj(ObjList(1))
-
-assert ls_one.get_data() == [1], "метод get_data вернул неверные данные"
-
-h = ls_one.head
-n = 0
-while h:
-    n += 1
-    h = h.get_next()
-    
-assert n == 1, "неверное число объектов в списке: возможно некорректно работает метод add_obj"
-ls_one.remove_obj()
-
-assert ls_one.get_data() == [], "метод get_data вернул неверные данные для пустого списка, возможно, неверно работает метод remove_obj"
-
-ls2 = LinkedList()
-assert ls.head != ls2.head, "атрибут head должен принадлежать объекту класса LinkedList, а не самому классу"
-assert ls.tail != ls2.tail, "атрибут tail должен принадлежать объекту класса LinkedList, а не самому классу"
-
-h = ls.head
-n = 0
-while h:
-    n += 1
-    h = h.get_next()
-    
-assert n == 4, "неверное число объектов в списке: возможно некорректно работает метод add_obj"
-
-h = ls.head
-n = 0
-while h:
-    h = h._ObjList__next
-    n += 1
-    
-assert n == 4, "неверное число объектов в списке: возможно некорректные значения в атрибутах __next"    
-
-h = ls.tail
-n = 0
-while h:
-    n += 1
-    h = h.get_prev()
-
-assert n == 4, "неверное число объектов в списке: возможно некорректно работает метод add_obj"
-
-h = ls.tail
-n = 0
-while h:
-    h = h._ObjList__prev
-    n += 1
-    
-assert n == 4, "неверное число объектов в списке: возможно некорректные значения в атрибутах __prev"
