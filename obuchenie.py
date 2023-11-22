1588e3d2ed7bb8462312f50bb67eee80beb3cff1
STUD <breath44@yande.ru>
2023-11-22 14:33:42 +0300

    12

--- obuchenie.py
+++ obuchenie.py
@@ -1,10 +1,41 @@
-def foo():
-    print('1')
-    yield
-    print('2')
-    yield
+# Может кому пригодиться))
+
+'''Общая схема работы дескриптора для класса Point3D'''
 
 
-for i in range(2):
-    next(foo())
+
+
+class Point3D:
+    class Integer:  # Дескриптор
+        def __set_name__(self, owner, name):
+            # Метод автоматически вызывается когда создается экземпляр класса
+            # self - ссылка на экземпляр класса, owner - ссылка на класс Point3D'''
+            self.name = '_' + name
+
+        def __get__(self, instance, owner):
+            # self - ссылка на экземпляр класса
+            # instance - ссылка на экзампляр класса из которого был вызван
+            # owner - ссылка на класс Point3D
+            return instance.__dict__[self.name]
+
+        def __set__(self, instance, value):
+            # Срабатывает в момент присваивания из инициализатора
+            # self - ссылка на экземпляр класса
+            # instance - ссылка на экзампляр класса из которого был вызван
+            # Value - значение которое будет присвоено
+            print(f"__set__:{self.name} = {value}")
+            instance.__dict__[self.name] = value
+
+    x = Integer()
+    y = Integer()
+    z = Integer()
+
+    def __init__(self, x, y, z):
+        # В инициализаторе идет обращение к дескрипторам x, y, z
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+pt = Point3D(1, 2, 3)
 
