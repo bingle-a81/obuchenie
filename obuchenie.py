c042738003d32e667d7aff4c68e79a9b4cbbe5ac
Al <breath44@yandex.ru>
2023-11-23 07:51:14 +0300

    22

--- obuchenie.py
+++ obuchenie.py
@@ -1,4 +1,22 @@
-a=''
-p=type(a)==type(a)
-print(p)
-print(type(p))
+class Bag:
+    def __init__(self, max_weight):
+        self.__max_weight=0
+        self.max_weight = max_weight
+        self.__things = []
+
+    @property
+    def max_weight(self):
+        return self.__max_weight
+    
+    @max_weight.setter
+    def max_weight(self, value):
+        if type(value) == int:
+            self.__max_weight = value
+
+    @property
+    def things(self):
+        return self.__things
+    
+    def add_thing(self, thing):
+        self.__things.append(thing)
+        self.max_weight += thing.weight
