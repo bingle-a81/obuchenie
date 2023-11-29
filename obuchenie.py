c283204d93064f8566cdd9de0484ccade1354fd7
Al <breath44@yandex.ru>
2023-11-29 22:18:24 +0300

    2

--- obuchenie.py
+++ obuchenie.py
@@ -1,44 +1,25 @@
-class Handler:
-    def __init__(self,methods:tuple,):
-     self.methods = methods
 
-    def __call__(self,func ) :
-        def wrapper(request:dict):
-            if request.get("method",'GET') in self.methods:
-                a=request.get("method",'GET').lower()
-                return self.__getattribute__(a)(func, request)
-            else:
-                return None
+class InputValues:
+    def __init__(self, render):     # render - ссылка на функцию или объект для преобразования
+        self.render=render
+
+    def __call__(self, func):     # func - ссылка на декорируемую функцию
+        def wrapper(*args, **kwargs):
+            return list(map(self.render,func().split()))
         return wrapper
+    
 
-    def get(self, func, request,):
-        return f'GET: {func(request)}'
+class RenderDigit:
+    def __call__(self, x) :
+            try:
+                a=int(x)
+                return a
+            except:
+                return None
+            
+@InputValues(RenderDigit())
+def input_dg():
+    return input()
 
-    def post(self, func, request,):
-        return f'POST: {func(request)}' 
-
-@Handler(methods=('GET', 'POST')) # по умолчанию methods = ('GET',)
-def contact(request):
-    return "Сергей Балакирев"      
-
-res = contact({"method": "POST", "url": "contact.html"})
+res = input_dg()
 print(res)
-
-assert hasattr(Handler, 'get') and hasattr(Handler, 'post'), "класс Handler должен содержать методы get и post"
-
-@Handler(methods=('GET', 'POST'))
-def contact2(request):
-    return "контакты"
-
-assert contact2({"method": "POST"}) == "POST: контакты", "декорированная функция вернула неверные данные"
-assert contact2({"method": "GET"}) == "GET: контакты", "декорированная функция вернула неверные данные"
-assert contact2({"method": "DELETE"}) is None, "декорированная функция вернула неверные данные"
-assert contact2({}) == "GET: контакты", "декорированная функция вернула неверные данные при указании пустого словаря"
-
-@Handler(methods=('POST'))
-def index(request):
-    return "index"
-
-assert index({"method": "POST"}) == "POST: index", "декорированная функция вернула неверные данные"
-assert index({"method": "GET"}) is None, "декорированная функция вернула неверные данные"
-assert index({"method": "DELETE"}) is None, "декорированная функция вернула неверные данные"
