849a322ce856016ad065445e91dcdfe9a207f035
STUD <breath44@yande.ru>
2023-12-01 17:35:33 +0300

    55

--- obuchenie.py
+++ obuchenie.py
@@ -1,30 +1,78 @@
-class PolyLine :
-    def __init__(self,*args) -> None:
-        self.coords=list(args)
-        
-    def add_coord(self,x, y):
-        self.coords.append((x,y))
+class NewList:
+    def __init__(self,ls:list=None) -> None:
+        if ls is None:
+            self.ls=[]
+        else:
+            self.ls=list(ls)
 
-    def remove_coord(self,indx):
-        self.coords.pop(indx)
+    def __str__(self) -> str:
+        return ' '.join([str(x) for x in self.ls])
 
-    def get_coords(self):
-        return self.coords
+    def get_list(self):
+        return self.ls
+
+    def __pop(self,a,b):
+        for x in b:
+            if x in a:
+                if type(x)==type(a[a.index(x)]):
+                    del(a[a.index(x)])
+                else:
+                    continue
+        return a
 
 
-poly = PolyLine((1, 2), (3, 5), (0, 10), (-1, 8))
-assert hasattr(poly, 'add_coord') and hasattr(poly, 'remove_coord') and hasattr(poly, 'get_coords') and \
-       callable(poly.add_coord) and callable(poly.remove_coord) and callable(poly.get_coords), \
-    "ошибка, не все методы есть в экземпляре класса"
+    def __sub__(self,other):
+        if isinstance(other,NewList):
+            b=other.ls
+        elif isinstance(other,list):
+            b=other
+        else:
+            TypeError
+        a=list(self.ls)
+        return NewList(self.__pop(a,b))
 
-poly.get_coords()
-assert poly.get_coords() == [(1, 2), (3, 5), (0, 10), (-1, 8)], \
-    "метод get_coords() вернул неправильный формат данных"
 
-poly.add_coord(10, 20)
-assert poly.get_coords()[-1] == (10, 20), "метод add_coord() работает некорректно"
+      
+    
+    def __rsub__(self,other):
+        a=list(self.ls)
+        return NewList(self.__pop(other,a))
 
-poly.remove_coord(0)
-poly.remove_coord(-1)
-assert poly.get_coords() == [(3, 5), (0, 10), (-1, 8)], "метод remove_coord() работает некорректно"
-print("Правильный ответ !!")
+    def __isub__(self,other):
+        if isinstance(other,NewList):
+            b=other.ls
+        elif isinstance(other,list):
+            b=other
+        else:
+            TypeError
+        a=list(self.ls)
+        return NewList(self.__pop(a,b))
+
+                
+
+lst = NewList()
+lst1 = NewList([0, 1, -3.4, "abc", True])
+lst2 = NewList([1, 0, True])
+
+
+assert lst1.get_list() == [0, 1, -3.4, "abc", True] and lst.get_list() == [], "метод get_list вернул неверный список"
+res1 = lst1 - lst2
+res2 = lst1 - [0, True]
+(print(res2))
+res3 = [1, 2, 3, 4.5] - lst2
+lst1 -= lst2
+
+assert res1.get_list() == [-3.4, "abc"], "метод get_list вернул неверный список"
+assert res2.get_list() == [1, -3.4, "abc"], "метод get_list вернул неверный список"
+
+assert res3.get_list() == [2, 3, 4.5], "метод get_list вернул неверный список"
+assert lst1.get_list() == [-3.4, "abc"], "метод get_list вернул неверный список"
+
+lst_1 = NewList([1, 0, True, False, 5.0, True, 1, True, -7.87])
+lst_2 = NewList([10, True, False, True, 1, 7.87])
+res = lst_1 - lst_2
+assert res.get_list() == [0, 5.0, 1, True, -7.87], "метод get_list вернул неверный список"
+
+a = NewList([2, 3])
+res_4 = [1, 2, 2, 3] - a # NewList: [1, 2]
+assert res_4.get_list() == [1, 2], "метод get_list вернул неверный список"
