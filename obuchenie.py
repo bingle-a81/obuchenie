91833e13f20e5cf2e9c63ea4a113d853e1009628
Al <breath44@yandex.ru>
2023-12-02 22:05:44 +0300

    55

--- obuchenie.py
+++ obuchenie.py
@@ -1,27 +1,49 @@
 class StackObj:
     def __init__(self,data,next=None):
-        self.__data = data
-        self.__next = next
+        self.data = data
+        self.next = next
 
 class Stack:
     def __init__(self) -> None:
-        self.__top=None
+        self.top=None
 
     def push_back(self, obj:StackObj):
-        if self.__top==None:
-            self.__top=obj
-            print(self.__top.next)
+        if self.top is None:
+            self.top=obj
+            return
+        n=self.top
+        while n.next is not None:
+            n=n.next
+        n.next=obj
+        return  
+    
+    def pop_back(self):
+        if self.top is None:
+            raise ValueError
+        if self.top.next is None:
+            self.top=None
+            return
         else:
-            self.__top.next=obj
-
-            # n=self.__top
-            # print(self.__top)
-            # while n:
-            #     n=n.next
-            # n.next=obj
+            n=self.top
+            while n.next is not None:
+                a=n
+                n=n.next
+            a.next=None
+            return
+    
+    def __len__(self):
+        i=0
+        n=self.top
+        while n:
+            i+=1
+            n=n.next
+        return i      
 
 
 st = Stack()
 st.push_back(StackObj('jj'))
 st.push_back(StackObj('jj'))
-print(st)
+st.push_back(StackObj('jj'))
+st.push_back(StackObj('jj'))
+st.pop_back()
+print(len(st))
