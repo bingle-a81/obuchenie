6e2c4b7043e120361137c9ffcd43a9786632d29a
STUD <breath44@yande.ru>
2023-12-04 17:07:57 +0300

    5

--- obuchenie.py
+++ obuchenie.py
@@ -1,21 +1,78 @@
-class Box3D:
-    def __init__(self,width,height,depth,):
-        self.width = width
-        self.height = height
-        self.depth = depth
+class MaxPooling:
+    def __init__(self,step=(2, 2), size=(2,2)) -> None:
+        self.step=step
+        self.size=size
 
-    def __add__(self,other):
-        return
+    def __check(self,matrix):
+        if not isinstance(matrix, list) or not all([len(row)==len(matrix[0]) for row in matrix]) or len(matrix) != len(matrix[0]):
+            raise ValueError("Неверный формат для первого параметра matrix.")  
+        for i in range(len(matrix[0])):
+            for j in range(len(matrix)):
+                if type(matrix[i][j]) not in (int,float):
+                    raise ValueError("Неверный формат для первого параметра matrix.")  
 
 
 
+    def __call__(self, matrix) :
+        lst = []
+        self.__check(matrix)
+        count = len(matrix)//self.step[0]
+        s = 0
+        for i in range(0, len(matrix), self.step[0]):
+            if count == s:
+                break
+            for j in range(0, len(matrix[i]), self.step[0]):
+                b = matrix[i][j:j+self.size[0]]
+                if len(b) == self.size[0]:
+                    lst.append(b)
+                c = matrix[i+1][j:j+self.size[0]]
+                if len(c) == self.size[0]:
+                    lst[-1].extend(c)
+            s += 1
+        res=[]        
+        lst1 = list(map(lambda x: max(x), lst))
+        for i in range(0, len(lst1),self.step[0]):
+            res.append(lst1[i:i+self.step[0]])
+        return res
 
-box1 = Box3D(1, 2, 3)
-box2 = Box3D(2, 4, 6)
 
-box = box1 + box2 # Box3D: width=3, height=6, depth=9 (соответствующие размерности складываются)
-box = box1 * 2    # Box3D: width=2, height=4, depth=6 (каждая размерность умножается на 2)
-box = 3 * box2    # Box3D: width=6, height=12, depth=18
-box = box2 - box1 # Box3D: width=1, height=2, depth=3 (соответствующие размерности вычитаются)
-box = box1 // 2   # Box3D: width=0, height=1, depth=1 (соответствующие размерности целочисленно делятся на 2)
-box = box2 % 3    # Box3D: width=2, height=1, depth=0
+mp = MaxPooling(step=(2, 2), size=(2,2))
+m1 = [[1, 10, 10], [5, 10, 0], [0, 1, 2]]
+m2 = [[1, 10, 10, 12], [5, 10, 0, -5], [0, 1, 2, 300], [40, -100, 0, 54.5]]
+res1 = mp(m1)
+res2 = mp(m2)
+
+assert res1 == [[10]], "неверный результат операции MaxPooling"
+assert res2 == [[10, 12], [40, 300]], "неверный результат операции MaxPooling"
+
+mp = MaxPooling(step=(3, 3), size=(2,2))
+m3 = [[1, 12, 14, 12], [5, 10, 0, -5], [0, 1, 2, 300], [40, -100, 0, 54.5]]
+res3 = mp(m3)
+assert res3 == [[12]], "неверный результат операции при MaxPooling(step=(3, 3), size=(2,2))"
+
+try:
+    res = mp([[1, 2], [3, 4, 5]])
+except ValueError:
+    assert True
+else:
+    assert False, "некорректо отработала проверка (или она отсутствует) на не прямоугольную матрицу"
+
+try:
+    res = mp([[1, 2], [3, '4']])
+except ValueError:
+    assert True
+else:
+    assert False, "некорректо отработала проверка (или она отсутствует) на не числовые значения в матрице"
+ 
+
+mp = MaxPooling(step=(1, 1), size=(5, 5))
+res = mp([[5, 0, 88, 2, 7, 65],
+          [1, 33, 7, 45, 0, 1],
+          [54, 8, 2, 38, 22, 7],
+          [73, 23, 6, 1, 15, 0],
+          [4, 12, 9, 1, 76, 6],
+          [0, 15, 10, 8, 11, 78]])    # [[88, 88], [76, 78]]
+
+assert res == [[88, 88], [76, 78]], "неверный результат операции MaxPooling(step=(1, 1), size=(5, 5))"
+
+    
