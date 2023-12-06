ef40a2efd5d6d64c0c00543a780e9f57d3e7da0b
Al <breath44@yandex.ru>
2023-12-06 08:55:23 +0300

    15

--- obuchenie.py
+++ obuchenie.py
@@ -8,27 +8,44 @@ class TrackLine:
         self.max_speed = max_speed
 
 
-
+@total_ordering
 class Track:
     def __init__(self,start_x=0,start_y=0,):
-        self.tr=[(start_x,start_y)]
+        self.tr=[TrackLine(start_x,start_y,0)]
 
-    def add_track(self, tr):
+    def add_track(self, tr:TrackLine):
         self.tr.append(tr)
 
     def get_tracks(self):
+        print (self.tr)
         return tuple(self.tr)
     
-    def __len__(self):
-        return int(sum([hypot(x[0],x[1]) for x.coords in self.tr]))
+    def __len__(self):     
+        res=(sum(self.__sl(self.tr[i-1].coords[0],self.tr[i-1].coords[1],self.tr[i].coords[0],self.tr[i].coords[1]) for i in range(1,len(self.tr))))
+        return int(res)
     
-    @total_ordering
-    def __eq__(self, __value: object) -> bool:
-        return 
+    def __sl(self,x1,y1,x2,y2):
+        return ((x1-x2)**2+(y1-y2)**2)**0.5
+    
+
+    def __eq__(self, __value) -> bool:
+        if not isinstance(__value,TrackLine):
+            AssertionError('')
+        return len(self)==len(__value)
+
+    def __gt__ (self, __value) -> bool:
+        if not isinstance(__value,TrackLine):
+            AssertionError('')    
+            print(len(self))    
+        return len(self)>len(__value)
+
         
 track1, track2 = Track(), Track(0, 1)
 track1.add_track(TrackLine(2, 4, 100))
 track1.add_track(TrackLine(5, -4, 100))
-print(track1.get_tracks())
+track2.add_track(TrackLine(3, 2, 90))
+track2.add_track(TrackLine(10, 8, 90))
 print(len(track1))
-
+print(len(track2))
+res_eq = track1 < track2
+print(res_eq)
