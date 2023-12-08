3d42e0197f0e017f4a13760d644cca3154f8031f
STUD <breath44@yande.ru>
2023-12-08 16:21:11 +0300

    55

--- obuchenie.py
+++ obuchenie.py
@@ -1,5 +1,39 @@
-i=4
-a=[[x for x in range(1,i+1)] for x in range(1,i+1)] 
+class Morph:
+    def __init__(self,*args) -> None:
+        if args is not None:
+            self._lst=set(args)
+        
+    def add_word(self, word):
+        if word.lower() not in self._lst:
+            self._lst.append(word.lower())
 
-for x in a:
-    print(x)
+    def get_words(self):
+        return tuple(self._lst)
+
+    def __eq__(self, __o:str) -> bool:
+        return __o.lower() in self._lst
+
+    def __ne__(self, __o: object) -> bool:
+        return __o.lower() not in self._lst
+
+
+st='''- связь, связи, связью, связей, связям, связями, связях
+- формула, формулы, формуле, формулу, формулой, формул, формулам, формулами, формулах
+- вектор, вектора, вектору, вектором, векторе, векторы, векторов, векторам, векторами, векторах
+- эффект, эффекта, эффекту, эффектом, эффекте, эффекты, эффектов, эффектам, эффектами, эффектах
+- день, дня, дню, днем, дне, дни, дням, днями, днях'''
+
+dict_words =[Morph(*[x.strip('-] ') for x in line.split(',')]) for line in st.splitlines()]
+
+a=Morph('связи')
+print(a=='СВЯЗИ')
+quit(-1)
+text = 'СВЯЗИ'   # эту строчку не менять
+text=text.lower().strip('.')
+ls=[]
+for x in set(text.split()):
+    for y in dict_words:
+        if x == y:
+            ls.append(x)
+
+print(len(ls))
