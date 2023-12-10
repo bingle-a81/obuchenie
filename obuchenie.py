2f9f7408bdc1249133d777a11c07d808a27ac00a
Al <breath44@yandex.ru>
2023-12-10 20:18:55 +0300

    102

--- obuchenie.py
+++ obuchenie.py
@@ -1,10 +1,13 @@
 class Thing:
     def __init__(self,name,mass,):
-        self.name = name
+        self.name = name.lower()
         self.mass = mass
         
     def __eq__(self, name:str) -> bool:
-        return self.name==name
+        if isinstance(name,Thing):
+            return self.name.lower()==name.name.lower()
+        else:
+            return self.name==name
     
     # def __ne__(self, name:str) -> bool:
     #     return self.name!=name
@@ -22,24 +25,22 @@ class Box:
         return self.ls
     
     def __eq__(self, __value) -> bool:
-        lo=self.ls[:]
-        for x in lo:
-            for y in __value.ls:
-                if x==y:
-                    lo.remove(x)
-        return len(lo)==0
+        if isinstance(__value, Box):
+            return all([True if x in __value.ls else False for x in self.ls])
 
 
 b1 = Box()
 b2 = Box()
 
 b1.add_thing(Thing('мел', 100))
-b1.add_thing(Thing('тряпка', 200))
+b1.add_thing(Thing('Тряпка', 200))
+b1.add_thing(Thing('Тряпка', 200))
 b1.add_thing(Thing('доска', 2000))
 
 b2.add_thing(Thing('тряпка', 200))
-b2.add_thing(Thing('мел', 100))
+b2.add_thing(Thing('Мел', 100))
+b2.add_thing(Thing('доска', 2000))
 b2.add_thing(Thing('доска', 2000))
 
-res = b1 == b2 # True
+res = b1 != b2 # True
 print(res)
