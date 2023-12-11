ac55f6cd862574a2f9c115f08867290209cb0330
Al <breath44@yandex.ru>
2023-12-11 08:40:48 +0300

    55

--- obuchenie.py
+++ obuchenie.py
@@ -3,17 +3,9 @@ class Thing:
         self.name = name.lower()
         self.mass = mass
         
-    def __eq__(self, name:str) -> bool:
-        if isinstance(name,Thing):
-            return self.name.lower()==name.name.lower()
-        else:
-            return self.name==name
+    def __eq__(self, thing) -> bool:
+        return (self.name.lower()==thing.name.lower()) and (self.mass==thing.mass)
     
-    # def __ne__(self, name:str) -> bool:
-    #     return self.name!=name
-
-
-
 class Box:
     def __init__(self) -> None:
         self.ls=[]
@@ -27,22 +19,20 @@ class Box:
     def __eq__(self, __value) -> bool:
         lo=self.ls[:]
         lo1=__value.ls[:]
-        for i,x in enumerate(self.ls):
+        for x in self.ls:
             print(x.name)
             for y in lo1:
                 if y==x:
                     lo1.remove(y)
-                else
-
-                    
-        return len(lo1)==0
+                    lo.remove(x)                   
+        return (len(lo1)==0) and (len(lo)==0)
 
 b1 = Box()
 b2 = Box()
 
 b1.add_thing(Thing('мел', 100))
-b1.add_thing(Thing('Тряпка', 200))
-b1.add_thing(Thing('Тряпка', 200))
+b1.add_thing(Thing('Тряпка', 2000))
+# b1.add_thing(Thing('Тряпка', 200))
 b1.add_thing(Thing('доска', 2000))
 
 b2.add_thing(Thing('тряпка', 200))
