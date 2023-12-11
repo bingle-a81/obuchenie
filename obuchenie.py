ac04f1fefba3895b6059703a2603278a3c1c8ce2
STUD <breath44@yande.ru>
2023-12-11 17:08:43 +0300

    22

--- obuchenie.py
+++ obuchenie.py
@@ -1,44 +1,29 @@
-class Thing:
-    def __init__(self,name,mass,):
-        self.name = name.lower()
-        self.mass = mass
-        
-    def __eq__(self, thing) -> bool:
-        return (self.name.lower()==thing.name.lower()) and (self.mass==thing.mass)
-    
-class Box:
-    def __init__(self) -> None:
-        self.ls=[]
+class Record:
+    pk=0
 
-    def add_thing(self, obj) :
-        self.ls.append(obj)
+    def __init__(self,fio, descr, old,) -> None:
+        __class__.pk+=1
+        self.pk=__class__.pk
+        self.fio=fio
+        self.descr=descr
+        self.old=int(old)
 
-    def get_things(self) :
-        return self.ls
-    
-    def __eq__(self, __value) -> bool:
-        lo=self.ls[:]
-        lo1=__value.ls[:]
-        for x in self.ls:
-            print(x.name)
-            for y in lo1:
-                if y==x:
-                    lo1.remove(y)
-                    lo.remove(x)                   
-        return (len(lo1)==0) and (len(lo)==0)
+    def __hash__(self) -> int:
+        return hash((self.fio,self.old))
 
-b1 = Box()
-b2 = Box()
+    def __eq__(self, __o: object) -> bool:
+        return hash(self)==hash(__o)
 
-b1.add_thing(Thing('мел', 100))
-b1.add_thing(Thing('Тряпка', 2000))
-# b1.add_thing(Thing('Тряпка', 200))
-b1.add_thing(Thing('доска', 2000))
 
-b2.add_thing(Thing('тряпка', 200))
-b2.add_thing(Thing('Мел', 100))
-# b2.add_thing(Thing('доска', 2000))
-b2.add_thing(Thing('доска', 2000))
+class DataBase :
 
-res = b1 == b2 # True
-print(res)
+    def __init__(self,path) -> None:
+        self.path=path
+        self.dict_db ={}
+
+    def write(self, record):
+        self.dict_db.setdefault(record,[]).append(hash(record))
+
+
+    def read(self, pk):
+        pass
