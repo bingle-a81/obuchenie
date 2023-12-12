1a836b6601802ee7148334a791f9b4bae446457f
Al <breath44@yandex.ru>
2023-12-12 23:31:21 +0300

    mistake

--- obuchenie.py
+++ obuchenie.py
@@ -1,58 +1,39 @@
+class Diskr:
+    @classmethod
+    def _check(cls,val):
+        if type(val) not in (int,float) or val<=0:
+            raise ValueError("длины сторон треугольника должны быть положительными числами")
+        return True
+
+
+    def __set_name__(self, owner, name):
+        self.name = '_'+name
+ 
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name,None)
+ 
+    def __set__(self, instance, value):
+        self._check(value)
+        if len(instance.__dict__)>1:
+            a=instance.__dict__.get('_a')
+            b=instance.__dict__.get('_b')
+            if any([a > value+b,  value > a+b, b > a+ value]):
+                raise ValueError("с указанными длинами нельзя образовать треугольник")
+        setattr(instance, self.name, value)
+
+
 class Triangle:
-    def __init__(self,a, b, c) -> None:  
-        self.__a=self.__b=self.__c=None      
+    a=Diskr()
+    b=Diskr()
+    c=Diskr()
+
+    def __init__(self,a, b, c) -> None:   
         self.a=a
         self.b=b
         self.c=c
 
-    def __is_triangle(self):
-        if all([self.a is not None,self.b is not None,self.c is not None]):
-            if any([self.a > self.b+self.c,  self.b > self.a+self.c, self.c > self.a+ self.b]):
-                raise ValueError("с указанными длинами нельзя образовать треугольник")
-        return 
-    
-    @property
-    def a(self):
-        return self.__a
-    @a.setter
-    def a(self, val):
-        self.__check(val)
-        if all([self.__b is not None,self.__c is not None]):
-            if any([val > self.b+self.c,  self.b > val+self.c, self.c > val+ self.b]):
-                raise ValueError("с указанными длинами нельзя образовать треугольник")
-        self.__a = val        
-
-    @property
-    def b(self):
-        return self.__b
-    @b.setter
-    def b(self, val):
-        self.__check(val)   
-        if all([self.a is not None,self.c is not None]):
-            if any([self.a > val+self.c,  val > self.a+self.c, self.c > self.a+ val]):
-                raise ValueError("с указанными длинами нельзя образовать треугольник")
-        self.__b = val
-
-
-    @property
-    def c(self):
-        return self.__c
-    @c.setter
-    def c(self, val):
-        self.__check(val)    
-        if all([self.a is not None,self.b is not None]):
-            if any([self.a > self.b+val,  self.b > self.a+val, val > self.a+ self.b]):
-                raise ValueError("с указанными длинами нельзя образовать треугольник")            
-        self.__c = val
-        self.__is_triangle()
-
     def __len__(self):
         return int(self.a+self.b+self.c)
-
-    def __check(self,__v):
-        if type(__v) not in (int,float) or __v<=0:
-            raise ValueError("длины сторон треугольника должны быть положительными числами")
-        return True
 
     def __call__(self) :
         p=(self.a+self.b+self.c)/2
@@ -60,18 +41,21 @@ class Triangle:
         return res
 
 
-tr = Triangle(5, 4, 3)
+# tr = Triangle(15, 4, 3)
+# print(tr.__dict__)
+# quit(-1)
+# tr = Triangle(15,5,6)
+
 try:
-    tr = Triangle(-15,5,6)
+    tr = Triangle(15,5,6)
 except ValueError:
     assert True
+    # print(tr.a)
 else:
     assert False, "не сгенерировалось исключение ValueEr"   
 
-print(tr.a)
 assert tr.a == 5 and tr.b == 4 and tr.c == 3, "дескрипторы вернули неверные значения"
-print(len(tr))
-# quit(-1)
+
 try:
     tr = Triangle(-5, 4, 3)
 except ValueError:
