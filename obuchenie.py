b4663c4c38f7fc680a4a17200122e5bc0f1c1017
STUD <breath44@yande.ru>
2023-12-13 16:19:56 +0300

    55

--- obuchenie.py
+++ obuchenie.py
@@ -1,77 +1,25 @@
-class Diskr:
-    @classmethod
-    def _check(cls,val):
-        if type(val) not in (int,float) or val<=0:
-            raise ValueError("длины сторон треугольника должны быть положительными числами")
-        return True
+import random
+
+class Cell:
+    def __init__(self,around_mines:int=0, mine:bool=False,fl_open:bool=False):
+        self.around_mines = around_mines
+        self.mine = mine
+        self.fl_open=fl_open
+
+class GamePole:
+    isis=None
+    def __new__(cls,*args,**kwargs) :
+        if cls.isis==None:
+            cls.isis=super().__new__(cls)
+        return cls.isis
+        
+    def __init__(self,N, M, total_mines):
+        self.N=N
+        self.M=M
+        self.__pole_cells=[[Cell() for i in range(self.N)] for j in range(self.N)]
+        self.pole=self.__pole_cells
 
 
-    def __set_name__(self, owner, name):
-        self.name = '_'+name
- 
-    def __get__(self, instance, owner):
-        return getattr(instance, self.name,None)
- 
-    def __set__(self, instance, value):
-        self._check(value)
-        if len(instance.__dict__)>1:
-            a=instance.__dict__.get('_a')
-            b=instance.__dict__.get('_b')
-            if any([a > value+b,  value > a+b, b > a+ value]):
-                raise ValueError("с указанными длинами нельзя образовать треугольник")
-        setattr(instance, self.name, value)
-
-
-class Triangle:
-    a=Diskr()
-    b=Diskr()
-    c=Diskr()
-
-    def __init__(self,a, b, c) -> None:   
-        self.a=a
-        self.b=b
-        self.c=c
-
-    def __len__(self):
-        return int(self.a+self.b+self.c)
-
-    def __call__(self) :
-        p=(self.a+self.b+self.c)/2
-        res = (p * (p-self.a) * (p-self.b) * (p-self.c))**0.5
-        return res
-
-
-# tr = Triangle(15, 4, 3)
-# print(tr.__dict__)
-# quit(-1)
-# tr = Triangle(15,5,6)
-
-try:
-    tr = Triangle(15,5,6)
-except ValueError:
-    assert True
-    # print(tr.a)
-else:
-    assert False, "не сгенерировалось исключение ValueEr"   
-
-assert tr.a == 5 and tr.b == 4 and tr.c == 3, "дескрипторы вернули неверные значения"
-
-try:
-    tr = Triangle(-5, 4, 3)
-except ValueError:
-    assert True
-else:
-    assert False, "не сгенерировалось исключение ValueError"
-
-
-try:
-    tr = Triangle(10, 1, 1)
-except ValueError:
-    assert True
-else:
-    assert False, "не сгенерировалось исключение ValueError"
-
-    
-tr = Triangle(5, 4, 3)
-assert len(tr) == 12, "функция len вернула неверное значение"
-assert 5.9 < tr() < 6.1, "метод __call__ вернул неверное значение"
+    @property
+    def pole(self):
+        return self.__pole
