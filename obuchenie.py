d3d6ac8b7758ef0a5fc92d748575d5dcf7817385
Al <breath44@yandex.ru>
2023-12-14 22:47:42 +0300

    2

--- obuchenie.py
+++ obuchenie.py
@@ -1,32 +1,7 @@
-class Record :
-    def __init__(self,**kwargs) -> None:
-        self.__dict__.update(kwargs)
-        
+import numpy as np
 
-    def __getitem__(self,item):
-        if item>len(self.__dict__)-1:
-            raise IndexError('неверный индекс поля') 
-        ls=list(self.__dict__.keys())       
-        return self.__dict__.get(ls[item])
-
-    def __setitem__(self,item,val):
-        if item>len(self.__dict__)-1:
-            raise IndexError('неверный индекс поля')
-        ls=list(self.__dict__.keys())
-        self.__dict__[ls[item]]=val
-            
+# n=int(input())
 
 
-pk=1
-print(type(pk))
-
-r = (pk=1, title='Python ООП', author='Балакирев',pk1=1, title1='Python ООП', author1='Балакирев')
-# r[0] = 2 # доступ к полю pk
-# r[1] = 'Супер курс по ООП' # доступ к полю title
-# r[2] = 'Балакирев С.М.' # доступ к полю author
-# print(r.__dict__)
-# print(r[1]) # Супер курс по ООП
-
-
-
-
+arr=np.full(5,1.)
+print(arr)
