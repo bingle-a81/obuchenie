f10c9d29353481a0ade304f926aba65116fea42d
Al <breath44@yandex.ru>
2023-12-17 22:35:52 +0300

    2

--- obuchenie.py
+++ obuchenie.py
@@ -1,104 +1,23 @@
-class StackObj:
-    def __init__(self,data,next=None):
-        self.data = data
-        self.next = next
-    @property
-    def next(self):
-        return self.__next
-    @next.setter
-    def next(self,val):
-        self.__next=val
-
-class Stack:
-    def __init__(self) -> None:
-        self.top=None
+class RadiusVector:
+    def __init__(self,*args):
+        self.coords=list(args)
 
     def __getitem__(self,item):
-        if 0>item or item>len(self)-1:
-            raise IndexError('неверный индекс')
-        n=self.top
-        i=0
-        while i<item:
-            n=n.next
-            i+=1
-        return n
-    
-    def __setitem__(self,item,obj:StackObj):
-        if 0>item or item>len(self)-1:
-            raise IndexError('неверный индекс')       
-        n=self.top
-        i=0
-        while i<item:
-            n=n.next
-            i+=1
-        n.data=obj.data     
-
-
-    def push(self, obj:StackObj):
-        if self.top is None:
-            self.top=obj
-            return
-        n=self.top
-        while n.next is not None:
-            n=n.next
-        n.next=obj
-        return  
-    
-    def pop(self):
-        if self.top is None:
-            raise ValueError
-        if self.top.next is None:
-            self.top=None
-            return
+        if type(item)==int:
+            return self.coords[item]
         else:
-            n=self.top
-            while n.next is not None:
-                a=n
-                n=n.next
-            a.next=None
-            return n
-        
+            return slice(self.coords)
 
-    def __len__(self):
-        i=0
-        n=self.top
-        while n:
-            i+=1
-            n=n.next
-        return i      
-    
-st = Stack()
-st.push(StackObj("obj1"))
-st.push(StackObj("obj2"))
-st.push(StackObj("obj3"))
-st[1] = StackObj("new obj2")
-print(st[2].data) # obj3
-print(st[1].data) # new obj2
+    def __setitem__(self,item,val):
+        if type(item)==int:
+            self.coords[item]=val
+        else:
+            print(item.start)
+            # self.coords[slice(item)]=val
 
-
-
-st = Stack()
-st.push(StackObj("obj11"))
-st.push(StackObj("obj12"))
-st.push(StackObj("obj13"))
-st[1] = StackObj("obj2-new")
-assert st[0].data == "obj11" and st[1].data == "obj2-new", "атрибут data объекта класса StackObj содержит неверные данные"
-
-try:
-    obj = st[3]
-except IndexError:
-    assert True
-else:
-    assert False, "не сгенерировалось исключение IndexError"
-
-obj = st.pop()
-assert obj.data == "obj13", "метод pop должен удалять последний объект стека и возвращать его"
-
-n = 0
-h = st.top
-while h:
-    assert isinstance(h, StackObj), "объект стека должен быть экземпляром класса StackObj"
-    n += 1
-    h = h.next
-    
-assert n == 2, "неверное число объектов в стеке (возможно, нарушена его структура)"
+v = RadiusVector(1, 1, 1, 1)
+print(v[1]) # 1
+v[:] = 1, 2, 3, 4
+print(v[2]) # 3
+print(v[1:]) # (2, 3, 4)
+v[0] = 10.5
