58b8b2cfeb1f5f2d7d9b5fb23d0d16d3b766c087
Al <breath44@yandex.ru>
2023-12-18 09:10:25 +0300

    v

--- obuchenie.py
+++ obuchenie.py
@@ -6,18 +6,15 @@ class RadiusVector:
         if type(item)==int:
             return self.coords[item]
         else:
-            return slice(self.coords)
+            return tuple(self.coords[item])
 
     def __setitem__(self,item,val):
-        if type(item)==int:
-            self.coords[item]=val
-        else:
-            print(item.start)
-            # self.coords[slice(item)]=val
+        self.coords[item]=val
 
 v = RadiusVector(1, 1, 1, 1)
 print(v[1]) # 1
 v[:] = 1, 2, 3, 4
+print(v[0])
 print(v[2]) # 3
 print(v[1:]) # (2, 3, 4)
 v[0] = 10.5
