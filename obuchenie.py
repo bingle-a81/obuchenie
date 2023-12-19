8901904e5d8529b724f32491bae5083a18027533
Al <breath44@yandex.ru>
2023-12-19 21:46:08 +0300

    g

--- obuchenie.py
+++ obuchenie.py
@@ -1,31 +1,18 @@
-class Cell:
-    def __init__(self,data) -> None:
-        self.data=data
+class GeomRange:
+    def __init__(self, start, step, stop):
+        self.start = start
+        self.step = step
+        self.stop = stop
+        self.__value = self.start
 
+    def __next__(self):
+        if self.__value < self.stop:
+            ret_value = self.__value
+            self.__value *= self.step
+            return ret_value
+        else:
+            raise StopIteration
 
-class SparseTable:
-    def __init__(self,rows=0,cols=0) -> None:
-        self.rows=rows
-        self.cols=cols
-        self.dict_={}
-
-    def add_data(self,row, col, data):
-        self.dict_[(row,col)]=data
-        pass
-        # raise IndexError('ячейка с указанными индексами не существует')
-
-    def remove_data(self,row, col) :
-        pass
-
-    def __getitem__(self,item):
-        pass
-
-    def __setitem__(self,item,val):
-        pass
-    # raise ValueError('данные по указанным индексам отсутствуют')
-
-
-
-st = SparseTable()
-st.add_data(2, 5, Cell("cell_25"))
-print(st.dict_)
+g = GeomRange(1, 1.2, 2)  
+res = next(g)
+res = next(g)
