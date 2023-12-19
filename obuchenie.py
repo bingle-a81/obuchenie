bdd9567b4777d76fdb760a9a0afeeb1aade7e11d
Al <breath44@yandex.ru>
2023-12-19 08:49:40 +0300

    4

--- obuchenie.py
+++ obuchenie.py
@@ -1,20 +1,31 @@
-class RadiusVector:
-    def __init__(self,*args):
-        self.coords=list(args)
+class Cell:
+    def __init__(self,data) -> None:
+        self.data=data
+
+
+class SparseTable:
+    def __init__(self,rows=0,cols=0) -> None:
+        self.rows=rows
+        self.cols=cols
+        self.dict_={}
+
+    def add_data(self,row, col, data):
+        self.dict_[(row,col)]=data
+        pass
+        # raise IndexError('ячейка с указанными индексами не существует')
+
+    def remove_data(self,row, col) :
+        pass
 
     def __getitem__(self,item):
-        if type(item)==int:
-            return self.coords[item]
-        else:
-            return tuple(self.coords[item])
+        pass
 
     def __setitem__(self,item,val):
-        self.coords[item]=val
+        pass
+    # raise ValueError('данные по указанным индексам отсутствуют')
 
-v = RadiusVector(1, 1, 1, 1)
-print(v[1]) # 1
-v[:] = 1, 2, 3, 4
-print(v[0])
-print(v[2]) # 3
-print(v[1:]) # (2, 3, 4)
-v[0] = 10.5
+
+
+st = SparseTable()
+st.add_data(2, 5, Cell("cell_25"))
+print(st.dict_)
