ce2c8ce7b988352479add3c85027ba6a399d4d4e
STUD <breath44@yande.ru>
2023-12-20 17:01:12 +0300

    3

--- obuchenie.py
+++ obuchenie.py
@@ -1,26 +1,38 @@
-class Person:
+class  TriangleListIterator:
     i=0
+    def __init__(self,lst) -> None:
+        self.lst=lst
 
-    def __init__(self,fio, job, old, salary, year_job):
-        self.fio,self.job,self.old,self.salary,self.year_job=fio, job, old, salary, year_job
-        self.lst=[self.fio,self.job,self.old,self.salary,self.year_job]
 
     def __iter__(self):
-        return self        
-    
-    def __next__(self):        
-        if self.i<len(self.lst):
-            res=self.lst[self.i]
+        return self
+
+    def __next__(self):
+        while self.i<len(self.lst):
+            j=0
+            while j<=self.i:   
+                # print (self.lst[self.i][self.i] )  
+                res=self.lst[self.i][j]     
+                j+=1     
+                yield(res )
             self.i+=1
-            return res
+            return 
         else:
             raise StopIteration
-    
 
 
-pers = Person('Гейтс Б.', 'бизнесмен', 61, 1000000, 46)
-# pers[0] = 'Балакирев С.М.'
-# print(pers[1])
-for v in pers:
-    print(v)
-# pers[5] = 123 # IndexError
+lst = [['x00', 'x01', 'x02'],
+       ['x10', 'x11'],
+       ['x20', 'x21', 'x22', 'x23', 'x24'],
+       ['x30', 'x31', 'x32', 'x33']]
+
+it = TriangleListIterator(lst)
+# print(next(it))
+# print(next(it))
+# print(next(it))
+
+for x in it:  # последовательный перебор всех элементов списка: x00, x10, x11, x20, ...
+    print(x)
+
+# it_iter = iter(it)
+# x = next(it_iter)
