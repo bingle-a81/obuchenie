5082dacb8cfcc9b40478cabf6cc4bcd974d395ad
Al <breath44@yandex.ru>
2023-12-20 08:55:08 +0300

    55

--- obuchenie.py
+++ obuchenie.py
@@ -1,18 +1,26 @@
-class GeomRange:
-    def __init__(self, start, step, stop):
-        self.start = start
-        self.step = step
-        self.stop = stop
-        self.__value = self.start
+class Person:
+    i=0
 
-    def __next__(self):
-        if self.__value < self.stop:
-            ret_value = self.__value
-            self.__value *= self.step
-            return ret_value
+    def __init__(self,fio, job, old, salary, year_job):
+        self.fio,self.job,self.old,self.salary,self.year_job=fio, job, old, salary, year_job
+        self.lst=[self.fio,self.job,self.old,self.salary,self.year_job]
+
+    def __iter__(self):
+        return self        
+    
+    def __next__(self):        
+        if self.i<len(self.lst):
+            res=self.lst[self.i]
+            self.i+=1
+            return res
         else:
             raise StopIteration
+    
 
-g = GeomRange(1, 1.2, 2)  
-res = next(g)
-res = next(g)
+
+pers = Person('Гейтс Б.', 'бизнесмен', 61, 1000000, 46)
+# pers[0] = 'Балакирев С.М.'
+# print(pers[1])
+for v in pers:
+    print(v)
+# pers[5] = 123 # IndexError
