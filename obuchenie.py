01a83efab106e76c15fd62b6f4879bf7c24315ad
Al <breath44@yandex.ru>
2023-12-21 08:25:42 +0300

    22

--- obuchenie.py
+++ obuchenie.py
@@ -1,32 +1,18 @@
-class  IterColumn:
+class Cell:
+    def __init__(self,data) -> None:
+        self.data=data
 
-    def __init__(self,lst,column) -> None:
-        self.lst=lst
-        self.column=column
-        self.i=0
-        self.temp=list(map(list,zip(*self.lst)))
+    @property
+    def data(self):
+        return self.data
+    @data.setter
+    def data(self,val):
+        self.data=val
 
-
-    def __iter__(self):
-        try:
-            for x in self.temp[self.column]:
-                yield(x )
-                
-        except StopIteration:
-            print('st')
-
+class TableValues:
+    def __init__(self,rows, cols, type_data) -> None:
+        self.rows=rows
+        self.cols=cols
+        self.type_data=type_data
+        
     
-
-
-
-lst = [['x00', 'x01', 'x02'],
-       ['x10', 'x11', 'x12'],
-       ['x20', 'x21', 'x22'],
-       ['x30', 'x31', 'x32']]
-
-it = IterColumn(lst, 1)
-for x in it:  # последовательный перебор всех элементов столбца списка: x12, x22, ..., xM2
-    print(x)
-
-it_iter = iter(it)
-x = next(it_iter)
