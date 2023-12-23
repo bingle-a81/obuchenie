76e0dfc137ca93e4a42b6e6655a91882da8c8c1f
Al <breath44@yandex.ru>
2023-12-23 09:52:33 +0300

    5

--- obuchenie.py
+++ obuchenie.py
@@ -1,69 +1,20 @@
-class Cell:
-    def __init__(self,data=0 ) -> None:
-        self.data =data 
+class Matrix:
+    def __init__(self,*args) -> None:
+        print(type(args))
+        if type(args)==tuple:
+            self._check(args)
+            self.ls=list(args)
+        else:
+            self.rows=args[0]
+            self.cols=args[1]
+            self.fill_value=args[2]
 
-    @property
-    def data(self):
-        return self.__data
-    @data.setter
-    def data(self, val):
-        self.__data = val
+    def _check(self,ls:tuple):
+        f=[x*2 for row in ls for x in row]
+        print(f)
 
-
-class TableValues :
-    def __init__(self,rows, cols, type_data=int) -> None:
-        self.rows=rows
-        self.cols=cols
-        self.type_data=type_data
-        self.table=tuple(tuple(Cell() for x in range(cols))for x in range(rows))
-
-    def __getitem__(self,item):
-        if not any((0<=item[0]<self.rows,0<=item[1]<self.cols)):
-            raise IndexError('неверный индекс')
-        return self.table[item[0]][item[1]].data
-
-    def __setitem__(self,item,val):
-        if not any((0<=item[0]<self.rows,0<=item[1]<self.cols)):
-            raise IndexError('неверный индекс')
-        if type(val)!=self.type_data:
-            raise TypeError('неверный тип присваиваемых данных')
-        self.table[item[0]][item[1]].data=val
-
-    def __iter__(self):
-        try:
-            for row in self.table:
-                yield (x.data for x in row)           
-        except StopIteration:
-            print('st')
-        return self
+        # if len(ls[0])!=len(ls[1]) and all(type(x) not in (int,float) for row in ls for x in row):
+            # raise TypeError('список должен быть прямоугольным, состоящим из чисел')
 
 
-tb = TableValues(3, 2)
-n = m = 0
-for row in tb:
-    n += 1
-    for value in row:
-        m += 1
-        assert type(value) == int and value == 0, "при переборе объекта класса TableValues с помощью вложенных циклов for, должен сначала возвращаться итератор для строк, а затем, этот итератор должен возвращать целые числа (значения соответствующих ячеек)"
-        
-assert n > 1 and m > 1, "неверно отработали вложенные циклы для перебора ячеек таблицы"
-
-
-tb[0, 0] = 10
-assert tb[0, 0] == 10, "не работает запись нового значения в ячейку таблицы"
-
-
-try:
-    tb[2, 0] = 5.2
-except TypeError:
-    assert True
-else:
-    assert False, "не сгенерировалось исключение TypeError"
-
-
-try:
-    a = tb[2, 4]
-except IndexError:
-    assert True
-else:
-    assert False, "не сгенерировалось исключение IndexError"
+mt = Matrix([[1, 2], [3, 4]])
