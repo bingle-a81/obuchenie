c78655f64ff58cf573b24dbf1a056874ddd9fd64
Al <breath44@yandex.ru>
2023-12-26 08:21:01 +0300

    2

--- obuchenie.py
+++ obuchenie.py
@@ -1,33 +1,26 @@
-class Thing:
-    def __init__(self,name, weight) -> None:
+class Animal:
+    def __init__(self,name, old) -> None:
         self.name=name
-        self.weight=weight
-
-class ArtObject(Thing):
-    def __init__(self, name, weight, author, date) -> None:
-        super().__init__(name, weight)
-        self.author=author
-        self.date=date
-
-class Computer(Thing):
-    def __init__(self, name, weight, memory, cpu) -> None:
-        super().__init__(name, weight)
-        self.memory=memory
-        self.cpu=cpu
-
-class Auto(Thing):
-    def __init__(self, name, weight,dims) -> None:
-        super().__init__(name, weight)
-        self.dims=dims
-
-class Mercedes(Auto):
-    def __init__(self, name, weight, dims, model, old) -> None:
-        super().__init__(name, weight, dims)
-        self.model=model
         self.old=old
 
-class Toyota(Auto):
-    def __init__(self, name, weight, dims,model, wheel) -> None:
-        super().__init__(name, weight, dims)
-        self.model=model
-        self.wheel=wheel
+class Cat(Animal):
+    def __init__(self, name, old, color, weight) -> None:
+        super().__init__(name, old)
+        self.color=color
+        self.weight=weight
+    
+    def get_info(self):
+        return f'{self.name}: {self.old}, {self.color},{self.weight}'
+
+class Dog(Animal):
+    def __init__(self, name, old, breed, size) -> None:
+        super().__init__(name, old)
+        self.breed = breed
+        self.size = size
+
+    def get_info(self):
+        return f'{self.name}: {self.old},{self.breed},{self.size}'
+
+
+cat = Cat('кот', 4, 'black', 2.25)
+print(cat.get_info())
