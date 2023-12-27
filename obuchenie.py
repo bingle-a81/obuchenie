1b0025463e9dd794126197af7f6a19a0de624108
STUD <breath44@yande.ru>
2023-12-27 16:51:26 +0300

    55

--- obuchenie.py
+++ obuchenie.py
@@ -1,27 +1,6 @@
-class Singleton:
-    __instance = None
-    __instance_base = None
+class Validator:
+    def _is_valid(self, data):
+        return True
 
-    def __new__(cls, *args, **kwargs):
-        if cls == Singleton:
-            if cls.__instance_base is None:
-                cls.__instance_base = super().__new__(cls)
-            return cls.__instance_base
-        if cls.__instance is None:
-            cls.__instance = super().__new__(cls)
-        return cls.__instance
-
-
-class Game(Singleton):
-    def __init__(self, name) -> None:
-        if "name" not in self.__dict__:
-            self.name = name
-
-
-t = Singleton("m")
-p1 = Game("first")
-p2 = Game("second")
-p3 = Game("3")
-print(p1.name)
-print(p2.name)
-print(p3.name)
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        pass
