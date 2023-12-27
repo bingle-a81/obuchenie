20ca1c5366c3152fae1314b03c743255d4ebdbd1
STUD <breath44@yande.ru>
2023-12-27 12:31:03 +0300

    55

--- obuchenie.py
+++ obuchenie.py
@@ -1,28 +1,27 @@
-class Animal:
-    def __init__(self, name, old) -> None:
-        self.name = name
-        self.old = old
+class Singleton:
+    __instance = None
+    __instance_base = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls == Singleton:
+            if cls.__instance_base is None:
+                cls.__instance_base = super().__new__(cls)
+            return cls.__instance_base
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
 
 
-class Cat(Animal):
-    def __init__(self, name, old, color, weight) -> None:
-        super().__init__(name, old)
-        self.color = color
-        self.weight = weight
-
-    def get_info(self):
-        return f"{self.name}: {self.old}, {self.color},{self.weight}"
+class Game(Singleton):
+    def __init__(self, name) -> None:
+        if "name" not in self.__dict__:
+            self.name = name
 
 
-class Dog(Animal):
-    def __init__(self, name, old, breed, size) -> None:
-        super().__init__(name, old)
-        self.breed = breed
-        self.size = size
-
-    def get_info(self):
-        return f"{self.name}: {self.old},{self.breed},{self.size}"
-
-
-cat = Cat("кот", 4, "black", 2.25)
-print(cat.get_info())
+t = Singleton("m")
+p1 = Game("first")
+p2 = Game("second")
+p3 = Game("3")
+print(p1.name)
+print(p2.name)
+print(p3.name)
