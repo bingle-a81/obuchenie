1be9bbc63a814b4cdbfa18c4c8dda35f8734af84
Al <breath44@yandex.ru>
2024-01-02 23:01:18 +0300

    55

--- obuchenie.py
+++ obuchenie.py
@@ -1,86 +1,38 @@
-class Vector:
-    def __init__(self, *args) -> None:
-        self.coords = list(args)
+class Router:
+    app = {}
 
-    def check(self, other):
-        if len(self.coords) != len(other.coords):
-            raise TypeError("размерности векторов не совпадают")
+    @classmethod
+    def get(cls, path):
+        return cls.app.get(path)
 
-    def __add__(self, other):
-        self.check(other)
-        return Vector(*list(map(lambda x: x[0] + x[1], zip(self.coords, other.coords))))
-
-    def __sub__(self, other):
-        self.check(other)
-        return __class__(
-            *list(map(lambda x: x[0] - x[1], zip(self.coords, other.coords)))
-        )
-
-    def get_coords(self):
-        return tuple(self.coords)
+    @classmethod
+    def add_callback(cls, path, func):
+        cls.app[path] = func
 
 
-class VectorInt(Vector):
-    def __init__(self, *args) -> None:
-        if any(type(x) != int for x in args):
-            raise ValueError("координаты должны быть целыми числами")
-        super().__init__(*args)
+class Callback:
+    def __init__(self, path, router_cls):
+        self.path = path
+        self.router_cls = router_cls
 
-    def __add__(self, other):
-        if isinstance(other, VectorInt):
-            self.check(other)
-            return __class__(
-                *list(map(lambda x: x[0] + x[1], zip(self.coords, other.coords)))
-            )
-        else:
-            return super().__sub__(other)
+    def __call__(self, func):
+        self.router_cls.add_callback(self.path, func)
 
-    def __sub__(self, other):
-        if isinstance(other, VectorInt):
-            self.check(other)
-            return __class__(
-                *list(map(lambda x: x[0] - x[1], zip(self.coords, other.coords)))
-            )
-        else:
-            return super().__sub__(other)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        return wrapper
 
 
-v1 = Vector(1, 2, 3)
-v2 = Vector(3, 4, 5)
-
-assert (v1 + v2).get_coords() == (
-    4,
-    6,
-    8,
-), "операция сложения дала неверные значения (или некорректно работает метод get_coords)"
-assert (v1 - v2).get_coords() == (
-    -2,
-    -2,
-    -2,
-), "операция вычитания дала неверные значения (или некорректно работает метод get_coords)"
-
-v = VectorInt(1, 2, 3, 4)
-assert isinstance(v, Vector), "класс VectorInt должен наследоваться от класса Vector"
-
-try:
-    v = VectorInt(1, 2, 3.4, 4)
-except ValueError:
-    assert True
-else:
-    assert (
-        False
-    ), "не сгенерировалось исключение ValueError для команды v = VectorInt(1, 2, 3.4, 4)"
+@Callback("/about", Router)
+def about():
+    return "<h1>About</h1>"
 
 
-v1 = VectorInt(1, 2, 3, 4)
-v2 = VectorInt(4, 2, 3, 4)
-v3 = Vector(1.0, 2, 3, 4)
+route = Router.get("/about")
+ret = route()
+print(ret)
+assert ret == "<h1>About</h1>", "декорированная функция вернула неверные данные"
 
-v = v1 + v2
-assert (
-    type(v) == VectorInt
-), "при сложении вектором с целочисленными координатами должен формироваться объект класса VectorInt"
-v = v1 + v3
-assert (
-    type(v) == Vector
-), "при сложении вектором с вещественными координатами должен формироваться объект класса Vector"
+route = Router.get("/")
+assert route is None, "Класс Router, при вызове метода get, вернул неверные данные"
