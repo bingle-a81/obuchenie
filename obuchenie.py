40d5d6d4b9b418b12b6aab50b673b639b74010e5
Al <breath44@yandex.ru>
2024-01-02 23:01:18 +0300

    55

--- obuchenie.py
+++ obuchenie.py
@@ -1,3 +1,41 @@
+class Router:
+    app = {}
+
+    @classmethod
+    def get(cls, path):
+        return cls.app.get(path)
+
+    @classmethod
+    def add_callback(cls, path, func):
+        cls.app[path] = func
+
+
+class Callback:
+    def __init__(self, path, router_cls):
+        self.path = path
+        self.router_cls = router_cls
+
+    def __call__(self, func):
+        self.router_cls.add_callback(self.path, func)
+
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        return wrapper
+
+
+@Callback("/about", Router)
+def about():
+    return "<h1>About</h1>"
+
+
+route = Router.get("/about")
+ret = route()
+print(ret)
+assert ret == "<h1>About</h1>", "декорированная функция вернула неверные данные"
+
+route = Router.get("/")
+assert route is None, "Класс Router, при вызове метода get, вернул неверные данные"
 class VideoItem:
     def __init__(self, title, descr, path) -> None:
         self.title = title
