9a1d8ea98d43a502a890161a3cae3432023af208
Al <breath44@yandex.ru>
2024-01-03 13:21:21 +0300

    2

--- obuchenie.py
+++ obuchenie.py
@@ -1,19 +1,33 @@
-class Router:
-    app = {}
+# здесь объявляйте функцию-декоратор
+def integer_params_decorated(func):
+    def wrap(self, *args, **kwargs):
+        for x in args:
+            if type(x) != int:
+                raise TypeError("аргументы должны быть целыми числами")
+        d = kwargs.values()
+        for x in d:
+            if type(x) != int:
+                raise TypeError("аргументы должны быть целыми числами")
+        return func(self, *args, **kwargs)
 
-    @classmethod
-    def get(cls, path):
-        return cls.app.get(path)
-
-    @classmethod
-    def add_callback(cls, path, func):
-        cls.app[path] = func
+    return wrap
 
 
-class Callback:
-    def __init__(self, path, router_cls):
-        self.path = path
-        self.router_cls = router_cls
+def integer_params(cls):
+    methods = {k: v for k, v in cls.__dict__.items() if callable(v)}
+    for k, v in methods.items():
+        setattr(cls, k, integer_params_decorated(v))
+
+    return cls
+
+
+@integer_params
+class Vector:
+    def __init__(self, *args):
+        self.__coords = list(args)
+
+    def __getitem__(self, item):
+        return self.__coords[item]
 
     def __call__(self, func):
         self.router_cls.add_callback(self.path, func)
