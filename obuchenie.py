f6a02414875c7e2e150af3e6eec37a0db3675839
STUD <breath44@yande.ru>
2024-01-06 14:50:04 +0300

    vv

--- obuchenie.py
+++ obuchenie.py
@@ -1,55 +1,4 @@
-# здесь объявляйте функцию-декоратор
-def integer_params_decorated(func):
-    def wrap(self, *args, **kwargs):
-        for x in args:
-            if type(x) != int:
-                raise TypeError("аргументы должны быть целыми числами")
-        d = kwargs.values()
-        for x in d:
-            if type(x) != int:
-                raise TypeError("аргументы должны быть целыми числами")
-        return func(self, *args, **kwargs)
-
-    return wrap
-
-
-def integer_params(cls):
-    methods = {k: v for k, v in cls.__dict__.items() if callable(v)}
-    for k, v in methods.items():
-        setattr(cls, k, integer_params_decorated(v))
-
-    return cls
-
-
-@integer_params
-class Vector:
-    def __init__(self, *args):
-        self.__coords = list(args)
-
-    def __getitem__(self, item):
-        return self.__coords[item]
-
-    def __call__(self, func):
-        self.router_cls.add_callback(self.path, func)
-
-        def wrapper(*args, **kwargs):
-            return func(*args, **kwargs)
-
-        return wrapper
-
-
-@Callback("/about", Router)
-def about():
-    return "<h1>About</h1>"
-
-
-route = Router.get("/about")
-ret = route()
-print(ret)
-assert ret == "<h1>About</h1>", "декорированная функция вернула неверные данные"
-
-route = Router.get("/")
-assert route is None, "Класс Router, при вызове метода get, вернул неверные данные"
+<<<<<<< HEAD
 class VideoItem:
     def __init__(self, title, descr, path) -> None:
         self.title = title
@@ -82,3 +31,48 @@ print(v.rating.rating)  # 5
 title = v.title
 descr = v.descr
 v.rating.rating = 6  # ValueError
+=======
+# здесь объявляйте функцию-декоратор
+def integer_params_decorated(func):
+    def wrap(self, *args, **kwargs):
+        for x in args:
+            if type(x) != int:
+                raise TypeError("аргументы должны быть целыми числами")
+        d = kwargs.values()
+        for x in d:
+            if type(x) != int:
+                raise TypeError("аргументы должны быть целыми числами")
+        return func(self, *args, **kwargs)
+
+    return wrap
+
+
+def integer_params(cls):
+    methods = {k: v for k, v in cls.__dict__.items() if callable(v)}
+    for k, v in methods.items():
+        setattr(cls, k, integer_params_decorated(v))
+
+    return cls
+
+
+@integer_params
+class Vector:
+    def __init__(self, *args):
+        self.__coords = list(args)
+
+    def __getitem__(self, item):
+        return self.__coords[item]
+
+    def __setitem__(self, key, value):
+        self.__coords[key] = value
+
+    def set_coords(self, *coords, reverse=False):
+        c = list(coords)
+        self.__coords = c if not reverse else c[::-1]
+
+
+vector = Vector(1, 2)
+vector.set_coords(1, 2, reverse=True)
+print(vector[1])
+vector[1] = 20.4  # TypeError
+>>>>>>> 6132451d5005603f02582403d77d0725f10b1ebf
