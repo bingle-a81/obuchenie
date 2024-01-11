63d675efeaf09c22e4f4dcb9dd42c30d897f8fab
Al <breath44@yandex.ru>
2024-01-11 22:05:35 +0300

    с

--- obuchenie.py
+++ obuchenie.py
@@ -1,29 +1,4 @@
-from abc import ABC, abstractmethod
-
-
-class Model(ABC):
-    @abstractmethod
-    def get_pk(self):
-        pass
-
-    def get_info(self):
-        return "Базовый класс Model"
-
-
-class ModelForm(Model):
-    _id = 0
-
-    def __init__(self, login, password) -> None:
-        __class__._id += 1
-        self._id = __class__._id
-        self.login = login
-        self.password = password
-
-    def get_pk(self):
-        return self._id
-
-
-form = ModelForm("Логин", "Пароль")
-form = ModelForm("Логин", "Пароль")
-form = ModelForm("Логин", "Пароль")
-print(form.get_pk())
+a = 5
+b = 10
+c = a + b
+print(c)
