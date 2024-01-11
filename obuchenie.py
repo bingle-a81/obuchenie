eac9d117206ec05902810a6a6e0c59894a507788
Al <breath44@yandex.ru>
2024-01-11 21:09:15 +0300

    f

--- obuchenie.py
+++ obuchenie.py
@@ -1,9 +1,29 @@
-from itertools import combinations
+from abc import ABC, abstractmethod
 
-answers = [
-    "2-й вариант предпочтительнее",
-    "2-й вариант вполне допустим",
-    "1-й предпочтительнее",
-    "1-й вариант вполне допустим",
-]
-print(*combinations(sorted(answers), 2), sep="\n")
+
+class Model(ABC):
+    @abstractmethod
+    def get_pk(self):
+        pass
+
+    def get_info(self):
+        return "Базовый класс Model"
+
+
+class ModelForm(Model):
+    _id = 0
+
+    def __init__(self, login, password) -> None:
+        __class__._id += 1
+        self._id = __class__._id
+        self.login = login
+        self.password = password
+
+    def get_pk(self):
+        return self._id
+
+
+form = ModelForm("Логин", "Пароль")
+form = ModelForm("Логин", "Пароль")
+form = ModelForm("Логин", "Пароль")
+print(form.get_pk())
