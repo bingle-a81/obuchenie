6523c216578f754b1c88caa4bc5761167e412532
Al <breath44@yandex.ru>
2024-01-13 21:47:06 +0300

    bb

--- obuchenie.py
+++ obuchenie.py
@@ -6,28 +6,29 @@ class Note:
         self._ton = ton
 
     @property
-    def name(self):
+    def _name(self):
         return self.__name
 
-    @name.setter
-    def name(self, __value):
+    @_name.setter
+    def _name(self, __value):
         if __value not in self.NOTE_LIST:
             raise ValueError("недопустимое значение аргумента")
         self.__name = __value
 
     @property
-    def ton(self):
+    def _ton(self):
         return self.__ton
 
-    @ton.setter
-    def ton(self, __value):
-        if type(__value) != int and __value not in range(-1, 1):
+    @_ton.setter
+    def _ton(self, __value):
+        if type(__value) != int or __value not in (-1, 0, 1):
             raise ValueError("недопустимое значение аргумента")
         self.__ton = __value
 
 
 class Notes:
     __slots__ = ("_do", "_re", "_mi", "_fa", "_solt", "_la", "_si")
+    NOTE_LIST = ("до", "ре", "ми", "фа", "соль", "ля", "си")
     __isictance = None
 
     def __new__(cls, *args, **kwargs):
@@ -35,7 +36,20 @@ class Notes:
             cls.__isictance = super().__new__(cls, *args, **kwargs)
         return cls.__isictance
 
+    def __init__(self) -> None:
+        for k, v in zip(self.__slots__, self.NOTE_LIST):
+            setattr(self, k, Note(v, 0))
+
     def __getitem__(self, item):
-        if not 0 <= item < 6:
+        if not 0 <= item < 7:
             raise IndexError("недопустимый индекс")
         return getattr(self, self.__slots__[item])
+
+
+notes = Notes()
+nota = notes[2]  # ссылка на ноту ми
+print(nota._ton)
+notes[6]._ton = 1  # изменение тональности ноты фа
+print(notes[6]._ton)
+n = Note("ми", 0)
+print(n._ton)
