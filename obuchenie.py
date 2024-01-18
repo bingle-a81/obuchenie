5b841ba4764713881cd0a635d3e7213cd3b56a41
STUD <breath44@yande.ru>
2024-01-18 17:34:51 +0300

    22

--- obuchenie.py
+++ obuchenie.py
@@ -2,13 +2,17 @@ from string import digits
 
 
 def func(f: str):
-    for i in list(f):
-        if i not in ("-" + digits):
-            return False
-    return True
+    try:
+        res = int(f)
+    except:
+        try:
+            res = float(f)
+        except:
+            res = str(f)
+    return res
 
 
 lst_in = "8 11 abcd -7.5 2.0 -5".split()
-print(sum(list(map(int, list(filter(lambda x: func(x), lst_in))))))
+print(list(map(lambda x: func(x), lst_in)))
 
 print("1" in digits)
