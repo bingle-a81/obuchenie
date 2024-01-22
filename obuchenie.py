768b4e8a7a94b64d628a7e95c01be0682d225b93
STUD <breath44@yande.ru>
2024-01-22 14:07:46 +0300

    5

--- obuchenie.py
+++ obuchenie.py
@@ -1,9 +1,16 @@
-from random import randint
+class Point:
+    def __init__(self, x=0, y=0) -> None:
+        self._x = x
+        self._y = y
 
 
-SIZE_GAME_POLE = 10
-
-
-class Ship:
-    ship_lst = []
-    
+a, b = input().split()
+try:
+    res = Point(int(a), int(b))
+except:
+    try:
+        res = Point(int(a), int(b))
+    except:
+        res = Point()
+finally:
+    print(f"Point: x = {res._x}, y = {res._y}")
