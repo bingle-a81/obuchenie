8c4e616e35a9d357ed6c9e0e9d425dbe5797b189
STUD <breath44@yande.ru>
2024-01-23 11:56:41 +0300

    h

--- obuchenie.py
+++ obuchenie.py
@@ -1,16 +1,27 @@
-class Point:
-    def __init__(self, x=0, y=0) -> None:
+class Rect:
+    def __init__(self, x, y, width, height) -> None:
         self._x = x
         self._y = y
+        self._width = width
+        self._height = height
+        self.left = self._x
+        self.right = self._x + self._width
+        self.up = self._y
+        self.bottom = self._y + self._height
+
+    def is_collision(self, rect):
+        if (
+            self.right < rect.left
+            or self.left > rect.right
+            or self.up > rect.bottom
+            or self.bottom < rect.up
+        ):
+            return True
+        raise TypeError("прямоугольники пересекаются")
 
 
-a, b = input().split()
-try:
-    res = Point(int(a), int(b))
-except:
-    try:
-        res = Point(int(a), int(b))
-    except:
-        res = Point()
-finally:
-    print(f"Point: x = {res._x}, y = {res._y}")
+lst_rect = [Rect(0, 0, 5, 3), Rect(6, 0, 3, 5), Rect(3, 2, 4, 4), Rect(0, 8, 8, 1)]
+# Сохранить их в списке lst_rect.
+# На основе списка lst_rect сформировать еще один список lst_not_collision,
+lst_not_collision = [lst_rect[-1]]
+print(lst_not_collision)
