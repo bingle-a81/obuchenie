ffd415fe46aecc93cb48b0d3d02b5e3d637a42e0
STUD <breath44@yande.ru>
2024-01-26 17:25:59 +0300

    55

--- obuchenie.py
+++ obuchenie.py
@@ -1,21 +1,14 @@
-class TupleLimit(tuple):
-    def __new__(cls, *args, **kwargs):
-        if len(args[0]) > args[1]:
-            raise ValueError('число элементов коллекции превышает заданный предел')
-        return super().__new__(cls, args[0])
-        
-    def __init__(self,lst,max) -> None:
-         super().__init__()
-         self.lst=lst
+class LimitException(Exception):
+    """Превышение лимита"""
 
 
-    def __str__(self) -> str:
-        return ' '.join(str(float(x)) for x in self.lst)
+class ServerLimitException(LimitException):
+    """Превышение нагрузки на сервер"""
 
-
-digits = list(map(float, input().split()))  # эту строчку не менять (коллекцию digits также не менять)
 
 try:
-    print(TupleLimit(digits,2))
-except ValueError as z:
-    print(z)
+    raise ServerLimitException("превышение серверной нагрузки")
+except LimitException:
+    print("LimitException")
+except ServerLimitException:
+    print("ServerLimitException")
