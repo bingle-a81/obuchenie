7b9d406b7ca3816915083037359a029e0e072f6f
Al <breath44@yandex.ru>
2024-03-26 21:30:32 +0300

    hash_file

--- obuchenie.py
+++ obuchenie.py
@@ -1,5 +1,26 @@
-import hashlib
 
-match "hello world":
-    case "hello world" as f:
-        print(hashlib.sha256(f.encode()).hexdigest())
+import hashlib
+# BUF_SIZE is totally arbitrary, change for your app!
+BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
+md51 = hashlib.sha1()
+md52= hashlib.sha1()
+with open('55.txt', 'rb') as f:
+    while True:
+        data = f.read(BUF_SIZE)
+        if not data:
+            break
+        md51.update(data)
+p1=md51.hexdigest()
+print(f"MD5: {p1}")
+
+
+with open('66.txt', 'rb') as f:
+    while True:
+        data = f.read(BUF_SIZE)
+        if not data:
+            break
+        md52.update(data)
+p2=md52.hexdigest()
+
+print(f"MD5: {p2}")
+print(p1==p2)
