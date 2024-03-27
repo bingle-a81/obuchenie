5051de7bda97abb2e5e07787005c8b8ca6ebd302
Al <breath44@yandex.ru>
2024-03-27 22:00:51 +0300

    numpy1

--- obuchenie.py
+++ obuchenie.py
@@ -1,26 +1,7 @@
-
-import hashlib
-# BUF_SIZE is totally arbitrary, change for your app!
-BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
-md51 = hashlib.sha1()
-md52= hashlib.sha1()
-with open('55.txt', 'rb') as f:
-    while True:
-        data = f.read(BUF_SIZE)
-        if not data:
-            break
-        md51.update(data)
-p1=md51.hexdigest()
-print(f"MD5: {p1}")
-
-
-with open('66.txt', 'rb') as f:
-    while True:
-        data = f.read(BUF_SIZE)
-        if not data:
-            break
-        md52.update(data)
-p2=md52.hexdigest()
-
-print(f"MD5: {p2}")
-print(p1==p2)
+import numpy as np
+# Создаем массив целых чисел
+arr = np.array(input(),dtype=None)
+print(arr)
+# Преобразуем его в массив с плавающей точкой
+arr_float = arr.astype(float)
+print(arr_float) # out: [1. 2. 3. 4. 5.]
