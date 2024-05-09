9b3f5c1b9eb563e1326fdbfcfde2251124d311ed
Al <breath44@yandex.ru>
2024-05-09 11:17:07 +0300

    вложенные циклы

--- obuchenie.py
+++ obuchenie.py
@@ -1,12 +1,8 @@
-import logging
-
-logging.basicConfig(level=10)
-a_log=logging.getLogger()
-print(a_log)
-
-ch=logging.StreamHandler()
-ch.setLevel(30)
-a_log.addHandler(ch)
-a_log.warning('jklo')
-
-print(a_log.handlers)
+st='w w w o r l d g g g g r e a t t e c c h e m g g p w w'
+ls=st.split()
+a=[]
+for i in ls:
+    
+    a.append(i)
+print(a)
+                
