75764e74fd29b7c06c2163774073e67a4f6c3a0f
work@ya.com <work>
2024-05-10 16:42:38 +0300

    55

--- obuchenie.py
+++ obuchenie.py
@@ -1,8 +1,17 @@
-st='w w w o r l d g g g g r e a t t e c c h e m g g p w w'
-ls=st.split()
-a=[]
+st = "g i v e t h h i i s m a a a n a g u u n"
+ls = st.split()
+a = []
+i1 = ls[0]
+a1 = 0
 for i in ls:
-    
-    a.append(i)
+    if i == i1:
+        a1 += 1
+    else:
+        a.append([i1] * a1)
+        a1 = 1
+    i1 = i
+else:
+    a.append([i] * a1)
+
+
 print(a)
-                
