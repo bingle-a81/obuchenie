6fa47b9f9a2ab0cf03b208417741dabbc718a4a9
work@ya.com <work>
2024-05-14 17:20:47 +0300

    matrix2

--- obuchenie.py
+++ obuchenie.py
@@ -1,10 +1,25 @@
-n=int(input())
-a=0
-st=[list(map(int,input().split())) for i in range(n)]
+# n = int(input())
+n = 4
+a1 = a2 = a3 = a4 = 0
 
+# st = [list(map(int, input().split())) for i in range(n)]
+st = [[1, 2, 3, 4], [5, 6, 7, 8], [3, 4, 5, 6], [1, 2, 3, 4]]
+
+max = st[0][0]
 for i in range(n):
     for j in range(n):
-        if i==j:
-            a+=st[i][j]
+        if i < j and i < n - 1 - j:
+            a1 += st[i][j]
+        elif i < j and i > n - 1 - j:
+            a2 += st[i][j]
+        elif i > j and i > n - 1 - j:
+            a3 += st[i][j]
+        elif i > j and i < n - 1 - j:
+            a4 += st[i][j]
 
-print(a)
+print(
+    f"""Верхняя четверть: {a1}
+Правая четверть: {a2}
+Нижняя четверть: {a3}
+Левая четверть: {a4}"""
+)
