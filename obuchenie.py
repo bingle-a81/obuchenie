80f0c28090fcdb95662ee1e31ee4db48871c816d
work@ya.com <work>
2024-05-14 21:46:14 +0300

    2

--- obuchenie.py
+++ obuchenie.py
@@ -1,11 +1,11 @@
 # n = int(input())
-n = 4
+n:int = 4
 a1 = a2 = a3 = a4 = 0
 
 # st = [list(map(int, input().split())) for i in range(n)]
 st = [[1, 2, 3, 4], [5, 6, 7, 8], [3, 4, 5, 6], [1, 2, 3, 4]]
 
-max = st[0][0]
+maxi: int = st[0][0]
 for i in range(n):
     for j in range(n):
         if i < j and i < n - 1 - j:
