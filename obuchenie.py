2c562dec4fd22f17b3fa00df8bc5bf737e7d9d57
work@ya.com <work>
2024-06-10 17:05:01 +0300

    9

--- obuchenie.py
+++ obuchenie.py
@@ -1,9 +1,7 @@
-
-a=input().split()
-
-b='1930'
-
-set_a=set(a)
-set_b=set(b)
-
-print('YES' if (set_a)==set_b else 'NO')
+myset = set()
+for i in range(10):
+    if i % 2 == 0:
+        myset.add('even')
+    else:
+        myset.add('odd')
+print((myset))
