bcc39f3e785945519dc8e4d9b8ae007b38fd2f04
work@ya.com <work>
2024-06-14 16:12:06 +0300

    ff

--- obuchenie.py
+++ obuchenie.py
@@ -1,10 +1,6 @@
-from functools import reduce
-import re
+keys = ['name', 'age', 'job']
+values = ['Timur', 28, 'Teacher']
 
-pat=r'\b\w+?\b'
-uo='Milk is white and so is glue, Ghosts are white and they say BOO!'
-b=set(re.findall(pat,uo))
-print(len(b))
+info = dict(zip(keys, values))
 
-# c=reduce(lambda a,b: set(a)|set(b),uo)
-# print(c)
+print(info)
