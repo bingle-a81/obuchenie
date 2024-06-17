52d8934345f5802b6f8888b83ed33ef6b923333f
work@ya.com <work>
2024-06-17 10:47:02 +0300

    ff

--- obuchenie.py
+++ obuchenie.py
@@ -1,6 +1,12 @@
-keys = ['name', 'age', 'job']
-values = ['Timur', 28, 'Teacher']
+dig = {
+    "CS101": {"audience_number": "3004", "teacher": "Хайнс", "time": "8:00"},
+    "CS102": {"audience_number": "4501", "teacher": "Альварадо", "time": "9:00"},
+    "CS103": {"audience_number": "6755", "teacher": "Рич", "time": "10:00"},
+    "NT110": {"audience_number": "1244", "teacher": "Берк", "time": "11:00"},
+    "CM241": {"audience_number": "1411", "teacher": "Ли", "time": "13:00"},
+}
+n = input()
 
-info = dict(zip(keys, values))
-
-print(info)
+ls = list(dig.get(n).values())
+print(f'{n}: ',end='')
+print(*ls,sep=', ')
