3fab761340fc2016144660925a9a2586c71fd968
work@ya.com <work>
2024-06-20 16:59:31 +0300

    g

--- obuchenie.py
+++ obuchenie.py
@@ -1,26 +1,9 @@
-s=input()
-
-d1={}
-for x in s:
-    d1.setdefault(x,0)
-    d1[x]+=1
-
-n=int(input())
-d={}
-for i in range(n):
-    a,b=input().split(':')
-    d[int(b)]=a
-
-ls=[]
-for i in s:
-    ls.append(d.get(d1.get(i)))
-
-print(*ls,sep='')
-
-
-
-
-
-
-
-
+def find_delitel(x:int):
+    s=[]
+    for i in range(1,x+1):
+        if x%i==0:
+            s.append(i)
+    return s
+numbers = [34, 10, 4, 6, 10, 23, 90, 100, 21, 35, 95, 1, 36, 38, 19, 1, 6, 87, 1000, 13456, 360]
+result = {x:find_delitel(x) for x in numbers}
+print(result)
