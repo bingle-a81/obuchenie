bb550b81a5131ab88cf72a598245325161a03047
work@ya.com <work>
2024-06-24 17:16:43 +0300

    3

--- obuchenie.py
+++ obuchenie.py
@@ -1,9 +1,10 @@
-def find_delitel(x:int):
-    s=[]
-    for i in range(1,x+1):
-        if x%i==0:
-            s.append(i)
-    return s
-numbers = [34, 10, 4, 6, 10, 23, 90, 100, 21, 35, 95, 1, 36, 38, 19, 1, 6, 87, 1000, 13456, 360]
-result = {x:find_delitel(x) for x in numbers}
-print(result)
+import random
+ls=[]
+while len(ls)<100:
+    for i in range(100):
+        a=random.randrange(1_000_010, 10_000_000)
+
+        if  a not in ls:
+            ls.append(a)
+
+print(*ls, sep="\n")
