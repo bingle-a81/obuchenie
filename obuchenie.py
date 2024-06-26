a9e71288bee884482e4a12eaf80b60e1e3a30b99
Al <breath44@yandex.ru>
2024-06-26 21:15:00 +0300

    22

--- obuchenie.py
+++ obuchenie.py
@@ -1,10 +1,31 @@
-import random
-ls=[]
-while len(ls)<100:
-    for i in range(100):
-        a=random.randrange(1_000_010, 10_000_000)
+import re
 
-        if  a not in ls:
-            ls.append(a)
+# st='''G0 X-0.200 Z10.000
+# G1 Z0.000
+# G1 X9.800
+# G2 X9.948 Z0.066 R0.200
+# G1 X23.370 Z14.851
+# G1 X40.664 Z24.835
+# G2 X40.764 Z25.008 R0.200
+# G1 Z45.008
+# G0 X50.764'''
 
-print(*ls, sep="\n")
+# pat=r'\w[\d\.-]+'
+
+# d=[]
+# for x in st.splitlines():
+#     d.append(x)
+# # print(d)
+
+
+# res=re.findall(pat,st)
+# print(res)
+
+
+st='Уфо по Воронежской области МО РФ  отделение 1 (короткое название, л/с 4578А521453 )'
+
+# pat=r'л[\.]?с[\s]?([\d]+)'
+pat=r'л[^\w]?с\s?([\d\w]+)'
+
+res=re.findall(pat,st)
+print(res)
