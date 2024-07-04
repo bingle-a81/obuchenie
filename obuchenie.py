53a2311723a2b49c852abfdeab4eef47abe38509
work@ya.com <work>
2024-07-04 13:48:11 +0300

    tsug

--- obuchenie.py
+++ obuchenie.py
@@ -1,10 +1,36 @@
-from decimal import *
+import re
+import pyperclip
 
-num = Decimal(input())
 
-a=num.as_tuple()
+def replace_sign(letter, text):
+    pat = letter + r"([\d\-\.]+)"
+    return re.sub(pat, lambda x: letter + str(float(x[1]) * (-1)), text)
 
-if len(a[1])/(-1*a[2])==1:
-    print(max(a[1]))
-else:
-    print(max(a[1])+min(a[1]))
+
+def halve_value_letter(letter, text) -> str:
+    pat = letter + r"([\d\-\.]+)"
+    return re.sub(pat, lambda x: letter + str(float(x[1]) / 2), text)
+
+
+def replace_letters(what_change, change_for, text) -> str:
+    return re.sub(what_change, change_for, text)
+
+
+def cross_substitution(ferst_change, second_change, text) -> str:
+    a = replace_letters(ferst_change, "@@", text)
+    a = replace_letters(second_change, ferst_change, a)
+    a = replace_letters("@@", second_change, a)
+    return a
+
+
+s: str = pyperclip.paste()
+res = ""
+for i in s.splitlines():
+    a: str = replace_letters("CM", "Y", i)
+    a = cross_substitution("G3", "G2", a)
+    a = replace_sign("Y", a)
+    a = halve_value_letter("X", a)
+    res += a + "\n"
+
+print(res)
+pyperclip.copy(res)
