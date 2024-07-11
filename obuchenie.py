95685495ecb83e7553a86a2c2ce3f50e6b5a5b0a
work@ya.com <work>
2024-07-11 14:34:37 +0300

    решение  прямых учет радиуса

--- obuchenie.py
+++ obuchenie.py
@@ -1,67 +1,107 @@
 import re
 
-s = """G0 X-0.200 Z10.000"""
-# G1 Z0.000
-# G1 X9.800
-# G2 X10.000 Z0.200 R0.200
-# G1 Z10.117
-# G1 X17.012 Z17.130
-# G2 X17.071 Z17.271 R0.200
-# G1 Z27.188
-# G1 X24.084 Z34.201
-# G2 X24.142 Z34.342 R0.200
-# G1 Z44.342
-# G0 X34.142
-# '''
-
+s = """G0 X-0.400 Z10.000
+G1 Z0.000
+G1 X19.600
+G2 X19.883 Z0.059 R0.200
+G1 X34.025 Z7.130
+G2 X34.089 Z7.171 R0.200
+G1 X44.089 Z15.831
+G2 X44.136 Z15.897 R0.200
+G1 X47.609 Z25.745
+G0 X67.609
+"""
 p = r"(\w)([\d\-\.]+)"
-
-ls = []
-d = dict()
-
-a = re.findall(p, s)
-d = {x[0]: x[1] for x in a}
+ls: list[dict[str, float]] = [
+    {x[0]: float(x[1]) for x in re.findall(p, x)} for x in s.splitlines()
+]
 
 
+ls_lines_points = []
+for i in range(1, len(ls)):
 
-print(d.get('X'))
-x11 = 0
-y11 = 20
-x12 = 5
-y12 = 20
+    p1 = ls[i - 1]
+    p2 = ls[i]
+    if p2.get("G") != 1:
+        continue
+    px1 = p1.get("X")
+    if px1 is None:
+        px1 = ls_lines_points[-1][0]
+    pz1 = p1.get("Z")
+    if pz1 is None:
+        pz1 = ls_lines_points[-1][1]
+    px2 = p2.get("X")
+    if px2 is None:
+        px2 = px1
+    pz2 = p2.get("Z")
+    if pz2 is None:
+        pz2 = pz1
+    line = (px1, pz1, px2, pz2)
 
-x21 = 20
-y21 = 20
-x22 = 28
-y22 = 21
-
-xx1 = x11 - x12
-yy1 = y11 - y12
-
-xx2 = x21 - x22
-yy2 = y21 - y22
-
-
-if xx1 == 0:
-    a1 = 1
-    b1 = 0
-    c1 = x11
-elif yy1 == 0:
-    a1 = 0
-    b1 = 1
-    c1 = y11
-else:
-    a1 = 1 / (xx1)
-    b1 = (-1) / (yy1)
-    c1 = (x11 / (xx1)) - (y11 / (yy1))
+    ls_lines_points.append(line)
 
 
-a2 = 1 / (xx2)
-b2 = (-1) / (yy2)
-c2 = (x21 / (xx2)) - (y21 / (yy2))
+print(ls_lines_points)
+# x11 = 0
+# y11 = 20
+# x12 = 5
+# y12 = 20
+
+# x21 = 20
+# y21 = 20
+# x22 = 28
+# y22 = 21
 
 
-x = (c1 * b2 - c2 * b1) / (a1 * b2 - a2 * b1)
-y = (c2 * a1 - c1 * a2) / (a1 * b2 - a2 * b1)
+def new_func(line1: tuple, line2: tuple) -> tuple[float, float]:
 
-print(x, y)
+    x11, y11, x12, y12 = line1
+    x21, y21, x22, y22 = line2
+
+    xx1: float = x11 - x12
+    yy1: float = y11 - y12
+
+    xx2: float = x21 - x22
+    yy2: float = y21 - y22
+
+    if xx1 == 0:
+        a1 = 1
+        b1 = 0
+        c1: float = x11
+    elif yy1 == 0:
+        a1 = 0
+        b1 = 1
+        c1 = y11
+    else:
+        a1: float = 1 / (xx1)
+        b1: float = (-1) / (yy1)
+        c1 = (x11 / (xx1)) - (y11 / (yy1))
+
+    if xx2 == 0:
+        a2 = 1
+        b2 = 0
+        c2: float = x21
+    elif yy2 == 0:
+        a2 = 0
+        b2 = 1
+        c2 = y21
+    else:
+        a2: float = 1 / (xx2)
+        b2: float = (-1) / (yy2)
+        c2: float = (x21 / (xx2)) - (y21 / (yy2))
+
+    res_x: float = (c1 * b2 - c2 * b1) / (a1 * b2 - a2 * b1)
+    res_y: float = (c2 * a1 - c1 * a2) / (a1 * b2 - a2 * b1)
+    return res_x, res_y
+
+
+x, y = new_func((-0.2, 10.0, -0.2, 0.0), (-0.2, 0.0, 9.8, 0.0))
+
+# print(x, y)
+for i in range(1, len(ls_lines_points)):
+    print(new_func(ls_lines_points[i - 1], ls_lines_points[i]))
+
+
+# print(new_func((-0.2, 0.0, 9.8, 0.0), (10.0, 0.2, 10.0, 10.117)))
+
+# print(new_func((1, 0.0, 5.0, 0.0), (10.000, 5.0, 10.0, 9.0)))
