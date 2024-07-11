1988c81cf00ae0d5f8984293db89be9b8dcaf0f5
work@ya.com <work>
2024-07-11 11:27:12 +0300

    perewsech prymih

--- obuchenie.py
+++ obuchenie.py
@@ -1,36 +1,67 @@
 import re
-import pyperclip
+
+s = """G0 X-0.200 Z10.000"""
+# G1 Z0.000
+# G1 X9.800
+# G2 X10.000 Z0.200 R0.200
+# G1 Z10.117
+# G1 X17.012 Z17.130
+# G2 X17.071 Z17.271 R0.200
+# G1 Z27.188
+# G1 X24.084 Z34.201
+# G2 X24.142 Z34.342 R0.200
+# G1 Z44.342
+# G0 X34.142
+# '''
+
+p = r"(\w)([\d\-\.]+)"
+
+ls = []
+d = dict()
+
+a = re.findall(p, s)
+d = {x[0]: x[1] for x in a}
 
 
-def replace_sign(letter, text):
-    pat = letter + r"([\d\-\.]+)"
-    return re.sub(pat, lambda x: letter + str(float(x[1]) * (-1)), text)
+
+print(d.get('X'))
+x11 = 0
+y11 = 20
+x12 = 5
+y12 = 20
+
+x21 = 20
+y21 = 20
+x22 = 28
+y22 = 21
+
+xx1 = x11 - x12
+yy1 = y11 - y12
+
+xx2 = x21 - x22
+yy2 = y21 - y22
 
 
-def halve_value_letter(letter, text) -> str:
-    pat = letter + r"([\d\-\.]+)"
-    return re.sub(pat, lambda x: letter + str(float(x[1]) / 2), text)
+if xx1 == 0:
+    a1 = 1
+    b1 = 0
+    c1 = x11
+elif yy1 == 0:
+    a1 = 0
+    b1 = 1
+    c1 = y11
+else:
+    a1 = 1 / (xx1)
+    b1 = (-1) / (yy1)
+    c1 = (x11 / (xx1)) - (y11 / (yy1))
 
 
-def replace_letters(what_change, change_for, text) -> str:
-    return re.sub(what_change, change_for, text)
+a2 = 1 / (xx2)
+b2 = (-1) / (yy2)
+c2 = (x21 / (xx2)) - (y21 / (yy2))
 
 
-def cross_substitution(ferst_change, second_change, text) -> str:
-    a = replace_letters(ferst_change, "@@", text)
-    a = replace_letters(second_change, ferst_change, a)
-    a = replace_letters("@@", second_change, a)
-    return a
+x = (c1 * b2 - c2 * b1) / (a1 * b2 - a2 * b1)
+y = (c2 * a1 - c1 * a2) / (a1 * b2 - a2 * b1)
 
-
-s: str = pyperclip.paste()
-res = ""
-for i in s.splitlines():
-    a: str = replace_letters("CM", "Y", i)
-    a = cross_substitution("G3", "G2", a)
-    a = replace_sign("Y", a)
-    a = halve_value_letter("X", a)
-    res += a + "\n"
-
-print(res)
-pyperclip.copy(res)
+print(x, y)
